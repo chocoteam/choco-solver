4ab7595c11375fed7c55b8354b547629d34fb2f1
Charles Prud'homme <cprudhom@mines-nantes.fr>
2012-02-06 11:25:00 +0100

    database: quick fix 4 -- add thread

--- src/shell/runner.py
+++ src/shell/runner.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python
 # CONSTANTS
 ## file name containing list of command to run
-import sys, subprocess, shlex, time, threading, os, signal,re, logging
+import sys, subprocess, shlex, time, threading, os, signal, re, logging
 from threading import Thread
 from os.path import join
+
 try:
     import MySQLdb
     import database
-except ImportError :
+except ImportError:
     database = None
-try :
+try:
     import matplotlib
     import pylab
     import plotmysql
 except ImportError:
     plotmysql = None
+
 
 #################################
 if database is None:
@@ -31,13 +33,19 @@ pwd = ''
 home = '../../'
 
 ## java command
-JAVA='java -Xmx1024m -Xms1024m '# -XX:+AggressiveOpts -XX:+UseConcMarkSweepG'
+JAVA = 'java -Xmx1024m -Xms1024m '# -XX:+AggressiveOpts -XX:+UseConcMarkSweepG'
 
 
 ## Number of time a problem is run
 loop = 1 # can be override
 ## time limit for a process
 timelimit = 180 #in seconds => 10min
+## number threads used
+thread = 1
+## insert in db
+db = (database is not None)
+## plot results
+plot = (database is not None) & (plotmysql is not None)
 
 ## regexp for statisctics
 ## [STATISTICS S Solutions, Objective: O, Resolution Ts (tms), N Nodes, B Backtracks, F Fails, R Restarts, P + P propagations]
@@ -45,21 +53,27 @@ timelimit = 180 #in seconds => 10min
 _SIZE = 15
 pattern = re.compile('\d+\,?\d*')
 _STAT = '[STATISTICS'
-_NAMES = 'SOLUTION','OBJECTIVE','TIME','NODE','BACKTRACK','FAIL','RESTART','PROPAGATIONS','SCRIPT_TIME'
+_NAMES = 'SOLUTION', 'OBJECTIVE', 'TIME', 'NODE', 'BACKTRACK', 'FAIL', 'RESTART', 'PROPAGATIONS', 'SCRIPT_TIME'
 
 
 def readParameters(paramlist):
     global loop
     global timelimit
+    global thread
     global name
     global dir
     global pwd
     global home
-    if len(paramlist)>0:
+    global db
+    global plot
+    offset = 2
+    if len(paramlist) > 0:
         if paramlist[0] == "-l": # option for number of time a problem must be run
             loop = int(paramlist[1])
         elif paramlist[0] == "-t":
             timelimit = int(paramlist[1]) # time limit before killing a process
+        elif paramlist[0] == "-j": # number of thread to use in a loop
+            thread = int(paramlist[1])
         elif paramlist[0] == "-n": # subname of output files
             name = paramlist[1]
         elif paramlist[0] == "-d": # directory of output files
@@ -68,20 +82,26 @@ def readParameters(paramlist):
             pwd = paramlist[1]
         elif paramlist[0] == "-h": # user pwd for db connexion
             home = paramlist[1]
-        readParameters(paramlist[2:])
+        elif paramlist[0] == "-nodb": # no database
+            db = False
+            offset = 1
+        elif paramlist[0] == "-noplot": # no plot
+            plot = False
+            offset = 1
+        readParameters(paramlist[offset:])
 
 
 readParameters(sys.argv[1:])
 print home
 
 # initialize env. variables
-CHOCO_SOLVER = join(home, 'solver','target',  'solver-rocs-1.0-SNAPSHOT-with-dep.jar')
-CP = '-cp .:'+CHOCO_SOLVER
-CMD = JAVA+' '+CP+' '+' samples.FrontEndBenchmarking'
+CHOCO_SOLVER = join(home, 'solver', 'target', 'solver-rocs-1.0-SNAPSHOT-with-dep.jar')
+CP = '-cp .:' + CHOCO_SOLVER
+CMD = JAVA + ' ' + CP + ' ' + ' samples.FrontEndBenchmarking'
 print CHOCO_SOLVER
 
 def buildLog(name, ext, level):
-    hdlr = logging.FileHandler(join(dir, name+ext))
+    hdlr = logging.FileHandler(join(dir, name + ext))
     formatter = logging.Formatter('%(message)s')
     hdlr.setFormatter(formatter)
     logger = logging.getLogger(name)
@@ -90,87 +110,69 @@ def buildLog(name, ext, level):
     return logger
 
 
-out = open(join(dir, name+'.txt'), 'w', 10)
+out = open(join(dir, name + '.txt'), 'w', 10)
 err = buildLog(name, '.log', logging.INFO)
 
 if len(sys.argv) > 1:
     if sys.argv[1] == "-l":
         loop = int(sys.argv[2])
-print ">> Run "+ str(loop) +" time(s)"
+print ">> Run " + str(loop) + " time(s)"
 
 def kill( process ):
     if process.poll() is None:
-        os.kill( process.pid, signal.SIGKILL )
+        os.kill(process.pid, signal.SIGKILL)
+
 
 def limit( process, cutoff ):
-    t = threading.Timer( cutoff, kill, [process] )
+    t = threading.Timer(cutoff, kill, [process])
     t.start()
     return t
 
-def compute(line, result, size):
-    b = []
-    if size == _SIZE:
-        b = _NAMES
-    else:
-        b = [_NAMES[0]]
-        b+= _NAMES[2:_SIZE]
-    out.write(line+"\n")
-    for i in range(size):
-        sum = 0.0
-        s = len(result[i])
-        result[i].sort()
-        if s > 2 :
-            result[i] = result[i][1:s-1]
-        for j in range(len(result[i])):
-            sum += result[i][j]
-        moy = round(sum/len(result[i]),6)
-        stdev = max(result[i]) - min(result[i])
-        info = "\t"+b[i] + ": "+ str(moy)
-        if stdev > 0:
-            info += " ("+str(stdev)+")"
-        out.write(info+'\n')
 
-def computeXLS(line, result, size):
+def computeXLS(line, result):
     line = line.rstrip("\n")
-    out.write(line+";")
-    for i in range(size):
+    out.write(line + ";")
+    for i in range(len(result)):
         sum = 0.0
         s = len(result[i])
         result[i].sort()
         if not s:
             return
-        if s > 2 :
-            result[i] = result[i][1:s-1]
+        if s > 2:
+            result[i] = result[i][1:s - 1]
 
         for j in range(len(result[i])):
             sum += result[i][j]
-        moy = round(sum/len(result[i]),6)
+        moy = round(sum / len(result[i]), 6)
         stdev = max(result[i]) - min(result[i])
-        info = ""+ str(moy)+";"
-        info += ""+str(stdev)+";;"
+        info = "" + str(moy) + ";"
+        info += "" + str(stdev) + ";;"
         out.write(info)
+    out.write('\n')
+    out.flush()
+
 
 def storeInDB(mydatab, line, results):
     parts = line.split(" ", 1) # separate name of problem and parameters
     parts = parts + [" "]
     mydatab.insertValues(parts[0], parts[1], results)
 
-f = open(join(dir, name+'.list'),'r')
+f = open(join(dir, name + '.list'), 'r')
 
 class runit(Thread):
-    def __init__ (self,args, i, j):
-      Thread.__init__(self)
-      self.args = args
-      self.results = [[]for k in range(_SIZE)]
-      self.i = i
-      self.j = j
-      self.s = _SIZE
+    def __init__ (self, args, i, j):
+        Thread.__init__(self)
+        self.args = args
+        self.results = [[]for k in range(_SIZE)]
+        self.i = i
+        self.j = j
+        self.s = _SIZE
 
     def run(self):
-        # print str(self.i)+'_'+str(self.j)
-        process = subprocess.Popen(args, bufsize=0, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
+    #        print str(self.i)+'_'+str(self.j)
+        process = subprocess.Popen(args, bufsize=0, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         start = time.time()
-        clock = limit( process, timelimit * loop )
+        clock = limit(process, timelimit * loop)
         process.wait()
         end = time.time()
         output, error = process.communicate()
@@ -180,56 +182,77 @@ class runit(Thread):
             out.write("error - see log file\n")
             err.error(args)
             for char in error:
-                if char =='\n':
+                if char == '\n':
                     err.error(line)
                     line = ''
                 else:
-                    line+= char
+                    line += char
             err.error('\n')
         else:
             for char in output:
-                if char =='\n':
+                if char == '\n':
                     data_line = line.split()
                     s = len(data_line)
-                    if s>0 and data_line[0] == '[STATISTICS':
+                    if s > 0 and data_line[0] == '[STATISTICS':
                         m = pattern.findall(line)
+                        #                        print line
                         self.s = len(m)
                         for j in range(self.s):
-                            self.results[j].append(float(m[j].replace(',','.')))
-                        self.results[len(m)].append(round((end-start)*1000, 2))
-                    line =''
+                            self.results[j].append(float(m[j].replace(',', '.')))
+                        self.results[len(m)].append(round((end - start) * 1000, 2))
+                    line = ''
                 else:
-                    line +=char
+                    line += char
 
         process.stdout.close()
         process.stderr.close()
 
 
 mydatab = None
-if database is not None:
+if (database is not None) & (db | plot):
     mydatab = database.Database('morini.emn.fr', 'choco-perf', pwd, 'choco-perf')
+
+if (database is not None) & db:
     mydatab.createTables()
     mydatab.openSession()
 
+## get commands per thread
+runlist = [[]]
+i = 0;
+j = 0
+runlist.append([])
 for line in f:
     if line[0] != '#' and line != '\n':
+        if i == len(runlist):
+            runlist.append([])
         line = line.rstrip("\n")
-        print line
-        command = CMD+' -loop '+ str(loop) +' -args "'+line + ' -log QUIET"'
+        runlist[i].append(line)
+        j += 1
+        if j == thread:
+            j = 0
+            i += 1
+
+print 'Start resolving'
+for i in range(len(runlist)):
+    runner = []
+    for t in range(len(runlist[i])):
+        line = runlist[i][t]
+        command = CMD + ' -loop ' + str(loop) + ' -args "' + line + ' -log QUIET"'
         args = shlex.split(command)
-
-        current  = runit(command, 0, 0)
+        current = runit(command, i, t)
+        runner.append(current)
         current.start()
-        current.join()
 
-#        compute(line, current.results, current.s)
-        computeXLS(line, current.results, current.s)
-        if database is not None:
-            storeInDB(mydatab, line, current.results)
-        out.write('\n')
-        out.flush()
+    for run in runner:
+        run.join()
+        computeXLS(line, run.results)
+        if (database is not None) & db:
+            storeInDB(mydatab, line, run.results)
+    print str((i + 1) * 100 / len(runlist)) + '% done'
+
 out.close()
-if database is not None & plotmysql is not None:
+
+if (database is not None) & (plotmysql is not None) & plot:
     mydatab.plot()
 
 print ">> End of script"
