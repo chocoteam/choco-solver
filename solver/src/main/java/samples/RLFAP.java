4a0b92ed14f269c9ef3c6765993d50d384f12def
Charles Prud'homme <cprudhom@mines-nantes.fr>
2012-07-12 22:38:56 +0200

    RLFAP again

--- solver/src/main/java/samples/RLFAP.java
+++ solver/src/main/java/samples/RLFAP.java
@@ -47,9 +47,6 @@ import solver.search.limits.LimitBox;
 import solver.search.loop.monitors.SearchMonitorFactory;
 import solver.search.restart.RestartFactory;
 import solver.search.strategy.StrategyFactory;
-import solver.search.strategy.selectors.values.InDomainMin;
-import solver.search.strategy.selectors.variables.DomOverWDegVS;
-import solver.search.strategy.strategy.Assignment;
 import solver.variables.IntVar;
 import solver.variables.VariableFactory;
 import solver.variables.view.Views;
@@ -82,7 +79,7 @@ public class RLFAP extends AbstractProblem {
     String dir = "/Users/cprudhom/Downloads/FullRLFAP/CELAR/scen02";
 
     @Option(name = "-o", aliases = "--optimize", usage = "Minimize the number of allocated frequencies", required = false)
-    boolean opt = true;
+    boolean opt = false;
 
     int[][] _dom, _ctr;
     int[][] _var;
@@ -176,9 +173,9 @@ public class RLFAP extends AbstractProblem {
 //        solver.set(StrategyFactory.minDomMinVal(vars, solver.getEnvironment()));
 //        solver.set(StrategyFactory.domddegMinDom(vars));
         if (true) {
-//            solver.set(StrategyFactory.domwdegMindom(vars, seed));
-            solver.set(new Assignment(vars, new DomOverWDegVS(vars, solver, seed),
-                    new InDomainMin()));
+            solver.set(StrategyFactory.domwdegMindom(vars, seed));
+//            solver.set(new Assignment(vars, new DomOverWDegVS(vars, solver, seed),
+//                    new InDomainMin()));
         } else {
             IntVar[] allvars = ArrayUtils.append(vars, cards, new IntVar[]{nb0});
             solver.set(StrategyFactory.ABSrandom(allvars, solver, 0.999d, 0.2d, 8, 1.1d, 1, seed));
