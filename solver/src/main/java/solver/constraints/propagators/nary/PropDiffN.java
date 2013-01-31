75118bebf8b1ad99c3895d73c84df0eca452cde7
Jean-Guillaume Fages <jg.fages@gmail.com>
2013-01-31 19:09:11 +0100

    diffn
    todo mzn builder

---
+++ solver/src/main/java/solver/constraints/propagators/nary/PropDiffN.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright (c) 1999-2012, Ecole des Mines de Nantes
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Ecole des Mines de Nantes nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package solver.constraints.propagators.nary;
+
+import choco.kernel.ESat;
+import choco.kernel.common.util.tools.ArrayUtils;
+import choco.kernel.memory.setDataStructures.ISet;
+import choco.kernel.memory.setDataStructures.SetFactory;
+import choco.kernel.memory.setDataStructures.SetType;
+import solver.Solver;
+import solver.constraints.Constraint;
+import solver.constraints.propagators.Propagator;
+import solver.constraints.propagators.PropagatorPriority;
+import solver.exception.ContradictionException;
+import solver.variables.EventType;
+import solver.variables.IntVar;
+import solver.variables.graph.UndirectedGraph;
+
+/**
+ * @author Jean-Guillaume Fages
+ * @since 31/01/13
+ */
+public class PropDiffN extends Propagator<IntVar> {
+
+	private int n;
+	private UndirectedGraph overlappingBoxes;
+	private ISet boxesToCompute;
+
+	public PropDiffN(IntVar[] x, IntVar[] y, IntVar[] dx, IntVar[] dy, Constraint<IntVar, Propagator<IntVar>> constraint, Solver solver) {
+		super(ArrayUtils.append(x,y,dx,dy), solver, constraint, PropagatorPriority.LINEAR, false);
+		n = x.length;
+		if(!(n==y.length && n==dx.length && n==dy.length)){
+			throw new UnsupportedOperationException();
+		}
+		overlappingBoxes = new UndirectedGraph(environment,n,SetType.LINKED_LIST,true);
+		boxesToCompute = SetFactory.makeStoredSet(SetType.LINKED_LIST,n,environment);
+	}
+
+	@Override
+	public int getPropagationConditions(int idx) {
+		return EventType.INSTANTIATE.mask + +EventType.BOUND.mask + EventType.REMOVE.mask;
+	}
+
+	@Override
+	public boolean advise(int varIdx, int mask) {
+		int v = varIdx%n;
+		ISet s = overlappingBoxes.getNeighborsOf(v);
+		for(int i=s.getFirstElement();i>=0;i=s.getNextElement()){
+			if(!overlap(v,i)){
+				overlappingBoxes.removeEdge(v,i);
+			}
+		}
+		if(!boxesToCompute.contain(v)){
+			boxesToCompute.add(v);
+		}
+		return super.advise(v,mask);
+	}
+
+	@Override
+	public void propagate(int evtmask) throws ContradictionException {
+		if ((evtmask & EventType.FULL_PROPAGATION.mask) != 0) {
+			for(int i=0;i<n;i++){
+				overlappingBoxes.getNeighborsOf(i).clear();
+			}
+			for(int i=0;i<n;i++){
+				for(int j=i+1;j<n;j++){
+					if(overlap(i,j)){
+						overlappingBoxes.addEdge(i,j);
+					}
+				}
+			}
+			boxesToCompute.clear();
+			for(int i=0;i<n;i++){
+				boxesToCompute.add(i);
+			}
+		}
+		for(int i=boxesToCompute.getFirstElement();i>=0;i=boxesToCompute.getFirstElement()){
+			boxesToCompute.remove(i);
+			filterFromBox(i);
+		}
+	}
+
+	private boolean overlap(int i, int j) {
+		if((disjoint(i,j,true))||(disjoint(i,j,false))
+				|| (disjoint(j,i,true))||(disjoint(j,i,false))){
+			return false;
+		}
+		return true;
+	}
+
+	private boolean disjoint(int i, int j, boolean x) {
+		int off = (x)?0:n;
+		if(vars[i+off].getLB()>=vars[j+off].getUB()+vars[j+off+2*n].getUB()){
+			return false;
+		}
+		return true;
+	}
+
+	protected void filterFromBox(int b) throws ContradictionException {
+		ISet s = overlappingBoxes.getNeighborsOf(b);
+		// mandatory part of box b
+		int earliestStart_xi = vars[b].getUB()-vars[b+2*n].getUB();
+		int earliestEnd_xi = vars[b].getLB()-vars[b+2*n].getLB();
+		int earliestStart_yi = vars[b+n].getUB()-vars[b+3*n].getUB();
+		int earliestEnd_yi = vars[b+n].getLB()-vars[b+3*n].getLB();
+		// mandatory part exists
+		if(earliestStart_xi<=earliestEnd_xi && earliestStart_yi<=earliestEnd_yi){
+			for(int j=s.getFirstElement();j>=0;j=s.getNextElement()){
+				filterBox(b,j,earliestStart_xi,earliestEnd_xi,earliestStart_yi,earliestEnd_yi);
+			}
+		}
+	}
+
+	private void filterBox(int i, int j, int es_xi, int ee_xi, int es_yi, int ee_yi) throws ContradictionException {
+		// mandatory part of box j
+		int earliestEnd_xj = vars[j].getLB()-vars[j+2*n].getLB();
+		int earliestStart_xj = vars[j].getUB()-vars[j+2*n].getUB();
+		int earliestEnd_yj = vars[j+n].getLB()-vars[j+3*n].getLB();
+		int earliestStart_yj = vars[j+n].getUB()-vars[j+3*n].getUB();
+		// mandatory part exists
+		if(earliestStart_xj<=earliestEnd_xj && earliestStart_yj<=earliestEnd_yj){
+			boolean overH = earliestStart_xj < ee_xi && earliestEnd_xj > es_xi;
+			boolean overV = earliestStart_yj < ee_yi && earliestEnd_yj > es_yi;
+			if(overH && overV){// mandatory parts overlap
+				contradiction(vars[i],"");
+			}
+			else if(overH){// mandatory parts overlap horizontally only
+				if(es_yi<earliestStart_yj){
+					vars[j+n].updateLowerBound(ee_yi,aCause);
+					vars[i+n].updateUpperBound(earliestStart_yj-vars[i+3*n].getLB(),aCause);
+					vars[i+3*n].updateUpperBound(earliestStart_yj-vars[i+n].getUB(),aCause);
+				}else{
+					vars[i+n].updateLowerBound(earliestEnd_yj,aCause);
+					vars[j+n].updateUpperBound(es_yi-vars[j+3*n].getLB(),aCause);
+					vars[j+3*n].updateUpperBound(es_yi-vars[j+n].getUB(),aCause);
+				}
+			}else if(overV){// mandatory parts overlap vertically only
+				if(es_xi<earliestStart_xj){
+					vars[j].updateLowerBound(ee_xi,aCause);
+					vars[i].updateUpperBound(earliestStart_xj-vars[i+2*n].getLB(),aCause);
+					vars[i+2*n].updateUpperBound(earliestStart_xj-vars[i].getUB(),aCause);
+				}else{
+					vars[i].updateLowerBound(earliestEnd_xj,aCause);
+					vars[j].updateUpperBound(es_xi-vars[j+2*n].getLB(),aCause);
+					vars[j+2*n].updateUpperBound(es_xi-vars[j].getUB(),aCause);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void propagate(int varIdx, int mask) throws ContradictionException {
+		forcePropagate(EventType.CUSTOM_PROPAGATION);
+	}
+
+	@Override
+	public ESat isEntailed() {
+		for(int i=0;i<n;i++){
+			if(boxInstantiated(i))
+				for(int j=i+1;j<n;j++){
+					if(overlap(i,j)){
+						if(boxInstantiated(j)){
+							return ESat.FALSE;
+						}
+					}
+				}
+		}
+		if (isCompletelyInstantiated()) {
+			return ESat.TRUE;
+		}
+		return ESat.UNDEFINED;
+	}
+
+	private boolean boxInstantiated(int i){
+		return vars[i].instantiated() && vars[i+n].instantiated()
+				&& vars[i+2*n].instantiated() && vars[i+3*n].instantiated();
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("DIFFN(");
+		sb.append("");
+		for (int i = 0; i < n; i++) {
+			if (i > 0) sb.append(",");
+			sb.append("["+vars[i].toString());
+			sb.append(","+vars[i+n].toString());
+			sb.append(","+vars[i+2*n].toString());
+			sb.append(","+vars[i+3*n].toString()+"]");
+		}
+		sb.append(")");
+		return sb.toString();
+	}
+
+}
--- solver/src/test/java/choco/checker/correctness/TestCorrectness.java
+++ solver/src/test/java/choco/checker/correctness/TestCorrectness.java
@@ -390,4 +390,14 @@ public class TestCorrectness {
 			}
 		}
 	}
+
+	@Test(groups = "1m")
+	public void testDiffn() {
+		for (int i = 0; i < 20; i++) {
+			long seed = System.currentTimeMillis();
+			for (int n = 2; n < 100; n *= 2) {
+				CorrectnessChecker.checkCorrectness(Modeler.modelDiffn, 4*n, 1,n/10, seed, true);
+			}
+		}
+	}
 }
