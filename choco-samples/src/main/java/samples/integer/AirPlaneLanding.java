38699ffb33bb3d0fc6937ef26cb80fcdac45fb17
cprudhom <cprudhom@mines-nantes.fr>
2013-08-30 17:18:13 +0200

    [LNS] LNSFactory plugs the LNS into the solver

--- choco-samples/src/main/java/samples/integer/AirPlaneLanding.java
+++ choco-samples/src/main/java/samples/integer/AirPlaneLanding.java
@@ -199,7 +199,7 @@ public class AirPlaneLanding extends AbstractProblem {
     @Override
     public void solve() {
         IntVar[] ivars = solver.retrieveIntVars();
-        solver.getSearchLoop().plugSearchMonitor(LNSFactory.pglns(solver, ivars, 30, 10, 200, 0, new FailCounter(100)));
+        LNSFactory.pglns(solver, ivars, 30, 10, 200, 0, new FailCounter(100));
         SMF.limitTime(solver, 5000); // because PGLNS is not complete (due to Fast Restarts), we add a time limit
         solver.findOptimalSolution(ResolutionPolicy.MINIMIZE, objective);
     }
