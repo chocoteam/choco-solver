c56329dc1f9db116efd08d3ec930fe14fcea2c29
cprudhom <cprudhom@mines-nantes.fr>
2014-02-03 14:27:42 +0100

    [Fix #92] done

--- bump_release.sh
+++ bump_release.sh
@@ -20,11 +20,16 @@ REPO_URL="http://www.emn.fr/z-info/choco-repo/mvn/repository/choco"
 case $1	in
 
 code)
+    DAT=`LANG=en_US.utf8 date +"%Y-%m"`
+    YEAR=`LANG=en_US.utf8 date +"%Y"`
+    d=`LANG=en_US.utf8 date +"%d %b %Y"`
 
 	## The README.md
 	# Update of the version number for maven usage
 
+    sedInPlace "s%Current stable version is .*.%Current stable version is $VERSION ($d).%"  README.md
 	sedInPlace "s%<version>.*</version>%<version>$VERSION</version>%"  README.md
+    sedInPlace "s%Choco3 is distributed.*.%Choco3 is distributed under BSD licence \(Copyright \(c\) 1999-$YEAR, Ecole des Mines de Nantes).%"  README.md
 
 	snapshot=0
 	echo $VERSION | grep "\-SNAPSHOT$" > /dev/null && snapshot=1
@@ -37,11 +42,14 @@ code)
 		sedInPlace "s%$REPO_URL.*choco\-solver.*%$REPO_URL/choco\-solver/$VERSION/choco\-solver\-$VERSION\-jar\-with\-dependencies\.jar%" README.md
 	fi
 	## The CHANGES.md file
-	d=`LANG=en_US.utf8 date +"%d %b %Y"`
 	REGEX="s%????*%${VERSION} - ${d}%"
 	sedInPlace "${REGEX}" CHANGES.md
 
-    echo -e "\n????\n-------------------\n" >> CHANGES.md
+    echo "\n????\n-------------------\n" >> CHANGES.md
+
+    ## The configuration file
+    sedInPlace "s%WELCOME_TITLE=.*%WELCOME_TITLE=** Choco $VERSION \($DAT\) : Constraint Programming Solver, Copyleft \(c\) 2010-$YEAR%"  choco-solver/src/main/resources/configuration.properties
+
 
 	;;
 	*)
