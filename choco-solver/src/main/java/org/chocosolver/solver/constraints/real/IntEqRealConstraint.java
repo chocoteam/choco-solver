5de4db7602811d5078cc6328867035d40cedb362
cprudhom <cprudhom@mines-nantes.fr>
2014-11-21 16:35:49 +0100

    #217: disable views per default

--- choco-solver/src/main/java/org/chocosolver/solver/constraints/real/IntEqRealConstraint.java
+++ choco-solver/src/main/java/org/chocosolver/solver/constraints/real/IntEqRealConstraint.java
@@ -27,8 +27,11 @@
 
 package org.chocosolver.solver.constraints.real;
 
+import gnu.trove.map.hash.THashMap;
+import org.chocosolver.solver.Solver;
 import org.chocosolver.solver.constraints.Constraint;
 import org.chocosolver.solver.constraints.Propagator;
+import org.chocosolver.solver.constraints.PropagatorPriority;
 import org.chocosolver.solver.exception.ContradictionException;
 import org.chocosolver.solver.variables.IntVar;
 import org.chocosolver.solver.variables.RealVar;
@@ -39,69 +42,104 @@ import org.chocosolver.util.tools.ArrayUtils;
 /**
  * Channeling constraint between integers and reals, to avoid views
  *
- * @author	Jean-Guillaume Fages
- * @since	07/04/2014
+ * @author Jean-Guillaume Fages
+ * @since 07/04/2014
  */
 public class IntEqRealConstraint extends Constraint {
 
-	//***********************************************************************************
-	// CONSTRUCTORS
-	//***********************************************************************************
+    //***********************************************************************************
+    // CONSTRUCTORS
+    //***********************************************************************************
 
-	/**
-	 * Channeling between integer variables intVars and real variables realVars.
-	 * Thus, for any i in [0,intVars.length-1], |intVars[i]-realVars[i]|< epsilon.
-	 * intVars.length must be equal to realVars.length.
-	 *
-	 * @param intVars	integer variables
-	 * @param realVars	real variables
-	 * @param epsilon	precision parameter
-	 */
-	public IntEqRealConstraint(final IntVar[] intVars, final RealVar[] realVars, final double epsilon) {
-		super("IntEqReal", new Propagator<Variable>(ArrayUtils.append(intVars,realVars)) {
-			int n = intVars.length;
-			@Override
-			public void propagate(int evtmask) throws ContradictionException {
-				assert n==realVars.length;
-				for(int i=0;i<n;i++) {
-					IntVar intVar = intVars[i];
-					RealVar realVar = realVars[i];
-					realVar.updateBounds((double) intVar.getLB() - epsilon, (double) intVar.getUB() + epsilon, aCause);
-					intVar.updateLowerBound((int) Math.ceil(realVar.getLB() - epsilon), aCause);
-					intVar.updateUpperBound((int) Math.floor(realVar.getUB() + epsilon), aCause);
-					if (intVar.hasEnumeratedDomain()) {
-						realVar.updateBounds((double) intVar.getLB() - epsilon, (double) intVar.getUB() + epsilon, aCause);
-					}
-				}
-			}
-			@Override
-			public ESat isEntailed() {
-				assert intVars.length==realVars.length;
-				boolean allInst = true;
-				for(int i=0;i<n;i++) {
-					IntVar intVar = intVars[i];
-					RealVar realVar = realVars[i];
-					if ((realVar.getLB() < (double) intVar.getLB() - epsilon) || (realVar.getUB() > (double) intVar.getUB() + epsilon)) {
-						return ESat.FALSE;
-					}
-					if (!(intVar.isInstantiated() && realVar.isInstantiated())) {
-						allInst = false;
-					}
-				}
-				return allInst?ESat.TRUE:ESat.UNDEFINED;
-			}
-		});
-	}
+    /**
+     * Channeling between integer variables intVars and real variables realVars.
+     * Thus, for any i in [0,intVars.length-1], |intVars[i]-realVars[i]|< epsilon.
+     * intVars.length must be equal to realVars.length.
+     *
+     * @param intVars  integer variables
+     * @param realVars real variables
+     * @param epsilon  precision parameter
+     */
+    public IntEqRealConstraint(IntVar[] intVars, RealVar[] realVars, double epsilon) {
+        super("IntEqReal", new PropIntEqReal(intVars, realVars, epsilon));
+    }
 
-	/**
-	 * Channeling between an integer variable intVar and a real variable realVar.
-	 * Thus, |intVar-realVar|< epsilon.
-	 *
-	 * @param intVar	integer variable
-	 * @param realVar	real variable
-	 * @param epsilon	precision parameter
-	 */
-	public IntEqRealConstraint(final IntVar intVar, final RealVar realVar, final double epsilon) {
-		this(new IntVar[]{intVar},new RealVar[]{realVar},epsilon);
-	}
+    /**
+     * Channeling between an integer variable intVar and a real variable realVar.
+     * Thus, |intVar-realVar|< epsilon.
+     *
+     * @param intVar  integer variable
+     * @param realVar real variable
+     * @param epsilon precision parameter
+     */
+    public IntEqRealConstraint(final IntVar intVar, final RealVar realVar, final double epsilon) {
+        this(new IntVar[]{intVar}, new RealVar[]{realVar}, epsilon);
+    }
+
+    private static class PropIntEqReal extends Propagator<Variable> {
+
+        int n;
+        IntVar[] intVars;
+        RealVar[] realVars;
+        double epsilon;
+
+        public PropIntEqReal(IntVar[] intVars, RealVar[] realVars, double epsilon) {
+            super(ArrayUtils.append(intVars, realVars), PropagatorPriority.LINEAR, false);
+            this.n = intVars.length;
+            this.intVars = intVars;
+            this.realVars = realVars;
+            this.epsilon = epsilon;
+            assert n == realVars.length;
+        }
+
+        @Override
+        public void propagate(int evtmask) throws ContradictionException {
+            for (int i = 0; i < n; i++) {
+                IntVar intVar = intVars[i];
+                RealVar realVar = realVars[i];
+                realVar.updateBounds((double) intVar.getLB() - epsilon, (double) intVar.getUB() + epsilon, aCause);
+                intVar.updateLowerBound((int) Math.ceil(realVar.getLB() - epsilon), aCause);
+                intVar.updateUpperBound((int) Math.floor(realVar.getUB() + epsilon), aCause);
+                if (intVar.hasEnumeratedDomain()) {
+                    realVar.updateBounds((double) intVar.getLB() - epsilon, (double) intVar.getUB() + epsilon, aCause);
+                }
+            }
+        }
+
+        @Override
+        public ESat isEntailed() {
+            assert intVars.length == realVars.length;
+            boolean allInst = true;
+            for (int i = 0; i < n; i++) {
+                IntVar intVar = intVars[i];
+                RealVar realVar = realVars[i];
+                if ((realVar.getLB() < (double) intVar.getLB() - epsilon) || (realVar.getUB() > (double) intVar.getUB() + epsilon)) {
+                    return ESat.FALSE;
+                }
+                if (!(intVar.isInstantiated() && realVar.isInstantiated())) {
+                    allInst = false;
+                }
+            }
+            return allInst ? ESat.TRUE : ESat.UNDEFINED;
+        }
+
+        @Override
+        public void duplicate(Solver solver, THashMap<Object, Object> identitymap) {
+            if (!identitymap.containsKey(this)) {
+                int size = this.n;
+
+                IntVar[] iVars = new IntVar[size];
+                for (int i = 0; i < size; i++) {
+                    this.vars[i].duplicate(solver, identitymap);
+                    iVars[i] = (IntVar) identitymap.get(this.vars[i]);
+                }
+                RealVar[] rVars = new RealVar[size];
+                for (int i = 0; i < size; i++) {
+                    this.vars[i + n].duplicate(solver, identitymap);
+                    rVars[i] = (RealVar) identitymap.get(this.vars[i + n]);
+                }
+                identitymap.put(this, new PropIntEqReal(iVars, rVars, epsilon));
+            }
+        }
+    }
 }
