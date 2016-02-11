42d107f932a2ca9b0ce825b8228f003ab2bb905d
Jean-Guillaume Fages <jg.fages@gmail.com>
2016-02-11 14:40:09 +0100

    reset engine in resolver.reset

--- choco-solver/src/main/java/org/chocosolver/solver/Resolver.java
+++ choco-solver/src/main/java/org/chocosolver/solver/Resolver.java
@@ -442,6 +442,7 @@ public final class Resolver implements Serializable, ISolver {
             action = initialize;
             mMeasures.reset();
             objectivemanager = SAT();
+            set(NoPropagationEngine.SINGLETON); // necessary
         }
     }
 
