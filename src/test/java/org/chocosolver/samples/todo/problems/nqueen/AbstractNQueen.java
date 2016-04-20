867a2533a2c16ebc6b902a1d2d54a99cd69d92a2
cprudhom <cprudhom@mines-nantes.fr>
2016-04-20 18:21:26 +0200

    wrong commit

--- src/test/java/org/chocosolver/samples/todo/problems/nqueen/AbstractNQueen.java
+++ src/test/java/org/chocosolver/samples/todo/problems/nqueen/AbstractNQueen.java
@@ -1,21 +1,21 @@
 /**
  * Copyright (c) 2015, Ecole des Mines de Nantes
  * All rights reserved.
- * <p>
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * 1. Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
+ *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
- * must display the following acknowledgement:
- * This product includes software developed by the <organization>.
+ *    must display the following acknowledgement:
+ *    This product includes software developed by the <organization>.
  * 4. Neither the name of the <organization> nor the
- * names of its contributors may be used to endorse or promote products
- * derived from this software without specific prior written permission.
- * <p>
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
  * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -30,7 +30,6 @@
 package org.chocosolver.samples.todo.problems.nqueen;
 
 import org.chocosolver.samples.AbstractProblem;
-import org.chocosolver.solver.Model;
 import org.chocosolver.solver.variables.IntVar;
 import org.kohsuke.args4j.Option;
 
@@ -55,24 +54,6 @@ public abstract class AbstractNQueen extends AbstractProblem {
 
     @Override
     public void solve() {
-        model.solve();
-    }
-
-    public static void main(String[] args) {
-        int n = 8;
-        Model model = new Model(n + "-queens problem");
-        IntVar[] vars = new IntVar[n];
-        for (int i = 0; i < vars.length; i++) {
-            vars[i] = model.intVar("Q_" + i, 1, n, false);
-        }
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int k = j - i;
-                model.arithm(vars[i], "!=", vars[j]).post();
-                model.arithm(vars[i], "!=", vars[j], "+", -k).post();
-                model.arithm(vars[i], "!=", vars[j], "+", k).post();
-            }
-        }
         model.solve();
     }
 }
