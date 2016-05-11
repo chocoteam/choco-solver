6f9660229094055a1cd9e5acfe01f838dcf2e5d6
Jean-Guillaume Fages <jg.fages@gmail.com>
2016-05-11 14:58:47 +0200

    fix test
    add test group
    call solve() from solver, not model
    update header

--- src/test/java/org/chocosolver/solver/variables/impl/SetVarImplTest.java
+++ src/test/java/org/chocosolver/solver/variables/impl/SetVarImplTest.java
@@ -1,5 +1,31 @@
 /**
+ * Copyright (c) 2015, Ecole des Mines de Nantes
+ * All rights reserved.
  *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    This product includes software developed by the <organization>.
+ * 4. Neither the name of the <organization> nor the
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package org.chocosolver.solver.variables.impl;
 
@@ -14,7 +40,7 @@ import org.testng.annotations.Test;
  */
 public class SetVarImplTest {
 
-	@Test
+	@Test(groups="1s", timeOut=60000)
 	public void testSetVarInstantiated() {
 		Model m = new Model();
 		Assert.assertTrue(m.setVar("var", new int[] { -1, -2, 3, 18 }).getCard().isInstantiatedTo(4));
@@ -22,7 +48,7 @@ public class SetVarImplTest {
 		Assert.assertTrue(m.setVar("var").getCard().isInstantiatedTo(0));
 	}
 
-	@Test
+	@Test(groups="1s", timeOut=60000)
 	public void testPropagate() {
 		Model m = new Model();
 		SetVar s = m.setVar("var", new int[] {}, new int[] { 0, 1, 2, 3 });
@@ -32,7 +58,7 @@ public class SetVarImplTest {
 		m.allDifferent(a, b, c, d).post();
 		// 4 different variables, among them two belong to s so c = 2. Which of a or b belong to s makes o difference
 		s.setCard(c);
-		Assert.assertTrue(m.solve());
+		Assert.assertTrue(m.getSolver().solve());
 		Assert.assertTrue(c.isInstantiatedTo(2), "" + c);
 	}
 
