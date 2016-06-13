b03951b03117ee508f94413f6a8b3f354356ef91
cprudhom <cprudhom@mines-nantes.fr>
2016-06-13 16:46:25 +0200

    initiate release 4.0.0.a

--- src/test/java/org/chocosolver/solver/constraints/nary/ScalarTest.java
+++ src/test/java/org/chocosolver/solver/constraints/nary/ScalarTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015, Ecole des Mines de Nantes
+ * Copyright (c) 2016, Ecole des Mines de Nantes
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
