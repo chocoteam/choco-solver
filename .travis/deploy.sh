27e707331f3cdf3f259d971dffceadee2212fd30
cprudhom <cprudhom@mines-nantes.fr>
2016-06-13 15:39:46 +0200

    prepare pre-release:  no javadoc/jar nor source.jar on snapshot release

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,10 +3,11 @@
 
 if [ ! -z "$TRAVIS_TAG" ]
 then
-    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    echo "on a tag -> set pom.xml <version> to ${TRAVIS_TAG}"
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=${TRAVIS_TAG} 1>/dev/null 2>/dev/null
+    SETTINGS=" javadoc:jar source:jar"
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
 fi
 
-mvn --settings .travis/settings.xml -P release clean javadoc:jar source:jar deploy -DskipTests -B -U
+mvn -s .travis/settings.xml -P release ${SETTINGS} deploy -DskipTests -B -U -q
