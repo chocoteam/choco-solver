29e6042033c60ba3bab1025c503ee8f66e93b288
cprudhom <cprudhom@mines-nantes.fr>
2016-09-22 15:47:36 +0200

    update header

--- src/main/java/org/chocosolver/solver/search/measure/MeasuresRecorder.java
+++ src/main/java/org/chocosolver/solver/search/measure/MeasuresRecorder.java
@@ -1,21 +1,21 @@
 /**
  * Copyright (c) 2016, Ecole des Mines de Nantes
  * All rights reserved.
- * <p>
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * 1. Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
+ *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
- * must display the following acknowledgement:
- * This product includes software developed by the <organization>.
+ *    must display the following acknowledgement:
+ *    This product includes software developed by the <organization>.
  * 4. Neither the name of the <organization> nor the
- * names of its contributors may be used to endorse or promote products
- * derived from this software without specific prior written permission.
- * <p>
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
  * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
