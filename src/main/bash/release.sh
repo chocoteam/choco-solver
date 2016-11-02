168767747752685a6db772042c62ee4938758c1a
cprudhom <cprudhom@mines-nantes.fr>
2016-11-02 10:59:19 +0100

    add release script

---
+++ src/main/bash/release.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+function getVersionToRelease() {
+    CURRENT_VERSION=`mvn ${MVN_ARGS} org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v "\[INFO\]"`
+    echo ${CURRENT_VERSION%%-SNAPSHOT}
+}
+
+function guess() {
+    v=$1
+    echo "${v%.*}.$((${v##*.}+1))-SNAPSHOT"
+}
+
+VERSION=$(getVersionToRelease)
+NEXT=$(guess $VERSION)
+TAG="samples-${VERSION}"
+
+git fetch
+git checkout -b release || exit 1
+
+mvn -q dependency:purge-local-repository
+
+echo "New version is ${VERSION}"
+#Update the poms:wq
+mvn versions:set -DnewVersion=${VERSION} -DgenerateBackupPoms=false
+git commit -m "initiate release ${VERSION}" -a
+
+echo "Start release"
+#Extract the version
+COMMIT=$(git rev-parse HEAD)
+
+#Quit if tag already exists
+git ls-remote --exit-code --tags origin ${TAG} && quit "tag ${TAG} already exists"
+
+#Working version ?
+# Well, we assume the tests have been run before, and everything is OK for the release
+#mvn clean test ||exit 1
+
+git fetch origin master:refs/remotes/origin/master||quit "Unable to fetch master"
+#Integrate with master and tag
+echo "** Integrate to master **"
+git checkout master ||quit "No master branch"
+git pull origin master || quit "Unable to pull master"
+git merge --no-ff -m "Merge tag '${TAG}' into master" ${COMMIT} ||quit "Unable to integrate to master"
+
+#NOT USED FOR THE MOMENT
+##Javadoc
+#./bin/push_javadoc apidocs.git ${VERSION}
+
+git tag ${TAG} ||quit "Unable to tag with ${TAG}"
+git push --tags ||quit "Unable to push the tag ${TAG}"
+git push origin master ||quit "Unable to push master"
+
+#    #Deploy the artifacts
+#echo "** Deploying the artifacts **"
+mvn -P release clean javadoc:jar source:jar deploy -DskipTests ||quit "Unable to deploy"
+
+mvn versions:set -DnewVersion=${NEXT} -DgenerateBackupPoms=false
+git commit -m "Prepare the code for the next version" -a ||quit "Unable to commit to master"
+#
+##Push changes on develop, with the tag
+git push origin master ||quit "Unable to push to develop"
+
+git checkout $TAG
+mvn clean install -DskipTests
+git checkout master
