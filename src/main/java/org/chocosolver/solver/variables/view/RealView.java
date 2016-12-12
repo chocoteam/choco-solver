68ba4dff7cabb5e4bf19ca3ee41a2aa0dcb71840
cprudhom <cprudhom@mines-nantes.fr>
2016-12-12 14:33:16 +0100

    fix #478

--- src/main/java/org/chocosolver/solver/variables/view/RealView.java
+++ src/main/java/org/chocosolver/solver/variables/view/RealView.java
@@ -89,7 +89,7 @@ public class RealView extends AbstractVariable implements IView, RealVar {
     @Override
     public boolean updateLowerBound(double value, ICause cause) throws ContradictionException {
         if (var.updateLowerBound((int) Math.ceil(value - precision), this)) {
-            notifyPropagators(RealEventType.INCLOW, cause);
+            super.notifyPropagators(RealEventType.INCLOW, cause);
             return true;
         }
         return false;
@@ -98,7 +98,7 @@ public class RealView extends AbstractVariable implements IView, RealVar {
     @Override
     public boolean updateUpperBound(double value, ICause cause) throws ContradictionException {
         if (var.updateUpperBound((int) Math.floor(value + precision), this)) {
-            notifyPropagators(RealEventType.INCLOW, cause);
+            super.notifyPropagators(RealEventType.INCLOW, cause);
             return true;
         }
         return false;
@@ -111,13 +111,13 @@ public class RealView extends AbstractVariable implements IView, RealVar {
         c += (var.updateUpperBound((int) Math.floor(upperbound + precision), this) ? 2 : 0);
         switch (c) {
             case 3:
-                notifyPropagators(RealEventType.BOUND, cause);
+                super.notifyPropagators(RealEventType.BOUND, cause);
                 return true;
             case 2:
-                notifyPropagators(RealEventType.DECUPP, cause);
+                super.notifyPropagators(RealEventType.DECUPP, cause);
                 return true;
             case 1:
-                notifyPropagators(RealEventType.INCLOW, cause);
+                super.notifyPropagators(RealEventType.INCLOW, cause);
                 return true;
             default: //cas 0;
                 return false;
@@ -149,6 +149,27 @@ public class RealView extends AbstractVariable implements IView, RealVar {
             monitors[i].onUpdate(this, event);
         }
     }
+
+    @Override
+    public void notifyPropagators(IEventType event, ICause cause) throws ContradictionException {
+        super.notifyPropagators(transformEvent((IntEventType) event), this);
+    }
+
+    public IEventType transformEvent(IntEventType evt) {
+        switch (evt){
+            case REMOVE:
+                throw new UnsupportedOperationException("Cannot transform REMOVE event from int to real");
+            case INCLOW:
+                return RealEventType.INCLOW;
+            case DECUPP:
+                return RealEventType.DECUPP;
+            default:
+            case INSTANTIATE:
+            case BOUND:
+                return RealEventType.BOUND;
+        }
+    }
+
 
     @Override
     public void contradiction(ICause cause, String message) throws ContradictionException {
