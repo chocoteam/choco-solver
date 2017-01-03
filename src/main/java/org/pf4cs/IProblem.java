857e9468c49cc0d04d83d0c416bd6a3aa19ff8db
cprudhom <cprudhom@mines-nantes.fr>
2017-01-03 17:28:44 +0100

    first commit

---
+++ src/main/java/org/pf4cs/IProblem.java
@@ -0,0 +1,81 @@
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2017, chocoteam
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.pf4cs;
+
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+
+/**
+ * A class that provides a pattern to declare a model and solve it. <br/>
+ * The methods are considered to be called in the following order:
+ * <pre> {@code this.setUp(args); // read program arguments
+ * this.buildModel(); // build the model (variables and constraints)
+ * this.configureSearch(); // configure the search strategy
+ * this.solve(); // launch the resolution process
+ * this.tearDown(); // run actions on exit}</pre>
+ *
+ * By default, {@link #setUp(String...)} is based on <a href="http://args4j.kohsuke.org/">args4j</a>.
+ *
+ * @author Charles Prud'homme
+ * @since 03/01/2017
+ */
+public interface IProblem extends IUpDown {
+
+    @Override
+    default void setUp(String... args) throws SetUpException{
+        CmdLineParser parser = new CmdLineParser(this);
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            System.err.println("java " + this.getClass() + " [options...]");
+            parser.printUsage(System.err);
+            System.err.println();
+            throw new SetUpException("Invalid problem options");
+        }
+    }
+
+    /**
+     * Call the model creation
+     */
+	void buildModel();
+
+    /**
+     * Call search configuration
+     */
+	void configureSearch();
+
+	/**
+     * Call problem resolution
+     */
+	void solve();
+}
