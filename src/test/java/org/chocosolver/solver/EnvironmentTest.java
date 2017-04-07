514d0f20772f2658a8dc115b772ab5849d620c41
Jean-Guillaume Fages <jg.fages@gmail.com>
2017-04-08 01:27:00 +0200

    test more env

--- src/test/java/org/chocosolver/solver/EnvironmentTest.java
+++ src/test/java/org/chocosolver/solver/EnvironmentTest.java
@@ -18,13 +18,14 @@ import org.chocosolver.memory.EnvironmentBuilder;
 import org.chocosolver.memory.trailing.EnvironmentTrailing;
 import org.chocosolver.memory.trailing.trail.chunck.ChunckedIntTrail;
 import org.chocosolver.memory.trailing.trail.flatten.StoredDoubleTrail;
+import org.chocosolver.solver.search.strategy.Search;
+import org.chocosolver.solver.variables.BoolVar;
 import org.chocosolver.solver.variables.IntVar;
+import org.chocosolver.solver.variables.SetVar;
+import org.chocosolver.util.tools.ArrayUtils;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import static org.chocosolver.solver.search.strategy.Search.domOverWDegSearch;
-import static org.chocosolver.util.tools.ArrayUtils.append;
 
 public class EnvironmentTest {
 
@@ -39,7 +40,7 @@ public class EnvironmentTest {
 
 	@Test(groups="10s", timeOut=60000, dataProvider = "env")
 	public void testSize(EnvironmentTrailing env) {
-		int n = 14;
+		int n = 12;
 		IntVar[] vars, vectors;
 		Model model = new Model(env, "CostasArrays");
 		vars = model.intVarArray("v", n, 0, n - 1, false);
@@ -57,10 +58,22 @@ public class EnvironmentTest {
 				}
 			}
 		}
+
+		// just to test more structures
+		BoolVar[][] bV = model.boolVarMatrix(n,n);
+		for (int i = 0; i < n; i++) {
+			model.boolsIntChanneling(bV[i],vars[i],0).post();
+		}
+		SetVar[] sV = model.setVarArray(n,new int[]{}, ArrayUtils.array(0,n-1));
+		for (int i = 0; i < n; i++) {
+			model.setBoolsChanneling(bV[i],sV[i]).post();
+		}
+
+
 		model.allDifferent(vars, "AC").post();
 		model.allDifferent(vectors, "BC").post();
 		Solver r = model.getSolver();
-		r.setSearch(domOverWDegSearch(append(vectors, vars)));
+		r.setSearch(Search.setVarSearch(sV));
 		model.getSolver().solve();
 	}
 
