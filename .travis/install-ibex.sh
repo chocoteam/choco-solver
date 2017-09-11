2834bcb2644b3a889c0431c2fb69f661813ee853
cprudhom <cprudhom@mines-nantes.fr>
2017-09-11 10:22:40 +0200

    try debug travis once again

--- .travis/install-ibex.sh
+++ .travis/install-ibex.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 set -ex
-wget http://www.ibex-lib.org/sites/default/files/ibex-2.3.4.tgz
-wget http://www.ibex-lib.org/sites/default/files/ibex-java-ibex2.3.tar.gz
+curl http://www.ibex-lib.org/sites/default/files/ibex-2.3.4.tgz > ibex-2.3.4.tgz
+curl http://www.ibex-lib.org/sites/default/files/ibex-java-ibex2.3.tar.gz > ibex-java-ibex2.3.tar.gz
 tar -xzvf ibex-2.3.4.tgz ibex-2.3.4/
 HIBEX=`pwd`
 mv ibex-java-ibex2.3.tar.gz ibex-2.3.4/plugins/ibex-java-ibex2.3.tar.gz
@@ -12,11 +12,7 @@ cd ..
 ./waf configure --enable-shared --with-jni --with-java-package=org.chocosolver.solver.constraints.real
 sudo ./waf install
 # to avoid error with LICENSE header update
-export LD_LIBRARY_PATH=/usr/local/lib
-cd __build__/src/java
-java Test
-cd ${HIBEX}
-mv __build__ ../ibex
-cd ..
-sudo rm -r ibex-2.3.4/
-sudo find ./ibex -name "*.java" -exec rm {} \;
+cd __build__/plugins/java/src
+java -Djava.library.path=/usr/local/lib Test
+cd ${HIBEX}/
+rm -r ibex-2.3.4/
