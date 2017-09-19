24291307d82729832e7446bd7124d80e6cecfcc8
cprudhom <cprudhom@mines-nantes.fr>
2017-09-19 09:53:47 +0200

    update travis script

--- .travis/install-ibex.sh
+++ .travis/install-ibex.sh
@@ -11,14 +11,7 @@ tar -xzvf ibex-java-ibex2.3.tar.gz
 cd ..
 ./waf configure --enable-shared --with-jni --with-java-package=org.chocosolver.solver.constraints.real
 sudo ./waf install
-ls -la /usr/local/lib/libibex*
-cat /etc/ld.so.conf
-sudo cp __build__/src/libibex.so /usr/local/lib/libibex.so
-sudo cp __build__/plugins/java/libibex-java.so /usr/local/lib/libibex-java.so
-cat /etc/ld.so.conf
-ldd /usr/local/lib/libibex-java.so
-ldconfig -v
-ldd /usr/local/lib/libibex-java.so
+export LD_LIBRARY_PATH=/usr/local/lib
 cd __build__/plugins/java/src
 java -Djava.library.path=/usr/local/lib Test
 cd ${HIBEX}/
