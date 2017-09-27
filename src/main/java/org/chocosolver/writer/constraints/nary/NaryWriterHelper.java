7c5a8f85788b46d16ea9275b56ad216013e8cbb4
cprudhom <cprudhom@mines-nantes.fr>
2017-09-27 16:26:38 +0200

    Update pom.xml + header

--- src/main/java/org/chocosolver/writer/constraints/nary/NaryWriterHelper.java
+++ src/main/java/org/chocosolver/writer/constraints/nary/NaryWriterHelper.java
@@ -1,10 +1,10 @@
 /**
- * This file is part of choco-solver, http://choco-solver.org/
+ * This file is part of choco-parsers, https://github.com/chocoteam/choco-parsers
  *
  * Copyright (c) 2017, IMT Atlantique. All rights reserved.
  *
- * Licensed under the BSD 4-clause license. See LICENSE file in the project root for full license
- * information.
+ * Licensed under the BSD 4-clause license.
+ * See LICENSE file in the project root for full license information.
  */
 package org.chocosolver.writer.constraints.nary;
 
--- src/test/java/org/chocosolver/parser/json/JSONConstraintTest.java
+++ src/test/java/org/chocosolver/parser/json/JSONConstraintTest.java
@@ -1,3 +1,11 @@
+/**
+ * This file is part of choco-parsers, https://github.com/chocoteam/choco-parsers
+ *
+ * Copyright (c) 2017, IMT Atlantique. All rights reserved.
+ *
+ * Licensed under the BSD 4-clause license.
+ * See LICENSE file in the project root for full license information.
+ */
 package org.chocosolver.parser.json;
 
 import org.chocosolver.solver.Model;
