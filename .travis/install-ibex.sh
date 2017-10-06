1b8642c969051c76cc8b779dffafa30fa35df995
cprudhom <cprudhom@mines-nantes.fr>
2017-10-06 15:36:43 +0200

    Update install-ibex.sh + .travis.yml

--- .travis/install-ibex.sh
+++ .travis/install-ibex.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 set -ex
-curl http://www.ibex-lib.org/sites/default/files/ibex-2.3.4.tgz > ibex-2.3.4.tgz
-curl http://www.ibex-lib.org/sites/default/files/ibex-java-ibex2.3.tar.gz > ibex-java-ibex2.3.tar.gz
-tar -xzf ibex-2.3.4.tgz ibex-2.3.4/
-HIBEX=`pwd`
-mv ibex-java-ibex2.3.tar.gz ibex-2.3.4/plugins/ibex-java-ibex2.3.tar.gz
-cd ibex-2.3.4/plugins
-tar -xzvf ibex-java-ibex2.3.tar.gz
-cd ..
-./waf configure --enable-shared --with-jni --with-java-package=org.chocosolver.solver.constraints.real
+# download Ibex and untar it
+curl http://www.ibex-lib.org/sites/default/files/ibex-2.5.3.tgz > ibex-2.5.3.tgz
+tar -xzf ibex-2.5.3.tgz
+# download java plugin
+curl http://www.ibex-lib.org/sites/default/files/ibex-java.tar.gz > ibex-java.tar.gz
+tar -xzf ibex-java.tar.gz -C ibex-2.5.3/plugins
+# prepare installation
+cd ibex-2.5.3
+./waf configure --enable-shared --with-jni --java-package-name=org.chocosolver.solver.constraints.real
 sudo ./waf install
 export LD_LIBRARY_PATH=/usr/local/lib
-cd __build__/plugins/java/src
-java -Djava.library.path=/usr/local/lib Test
-cd ${HIBEX}/
-sudo rm -r ibex-2.3.4.tgz
-sudo rm -r ibex-2.3.4/
+#cd __build__/plugins/java/src
+#java -Djava.library.path=/usr/local/lib Test
+cd -
+sudo rm -r ibex-2.5.3.tgz
+sudo rm -r ibex-2.5.3
 
