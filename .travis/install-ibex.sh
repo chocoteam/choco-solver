3780c36b579aeea7440d4cc38c7723a0cfa12f02
cprudhom <cprudhom@mines-nantes.fr>
2017-10-09 11:07:52 +0200

    Move to Ibex-2.6.0

--- .travis/install-ibex.sh
+++ .travis/install-ibex.sh
@@ -2,19 +2,20 @@
 
 set -ex
 # download Ibex and untar it
-curl http://www.ibex-lib.org/sites/default/files/ibex-2.5.3.tgz > ibex-2.5.3.tgz
-tar -xzf ibex-2.5.3.tgz
+curl http://www.ibex-lib.org/sites/default/files/ibex-2.6.0.tgz > ibex-2.6.0.tgz
+tar -xzf ibex-2.6.0.tgz
 # download java plugin
 curl http://www.ibex-lib.org/sites/default/files/ibex-java.tar.gz > ibex-java.tar.gz
-tar -xzf ibex-java.tar.gz -C ibex-2.5.3/plugins
+tar -xzf ibex-java.tar.gz -C ibex-2.6.0/plugins
 # prepare installation
-cd ibex-2.5.3
+cd ibex-2.6.0
 ./waf configure --enable-shared --with-jni --java-package-name=org.chocosolver.solver.constraints.real
 sudo ./waf install
 export LD_LIBRARY_PATH=/usr/local/lib
 #cd __build__/plugins/java/src
 #java -Djava.library.path=/usr/local/lib Test
 cd -
-sudo rm -r ibex-2.5.3.tgz
-sudo rm -r ibex-2.5.3
+sudo rm -r ibex-2.6.0.tgz
+sudo rm -r ibex-2.6.0
+sudo rm -r ibex-java.tar.gz
 
--- README.md
+++ README.md
@@ -174,7 +174,10 @@ The required dependencies for compilation are:
 
 
 
-They are available on Maven Repository (http://mvnrepository.com/).
+They are available on [Maven Repository](http://mvnrepository.com/).
+
+To declare continuous constraints, [Ibex-2.6.0](http://www.ibex-lib.org/download) needs to be installed
+(instructions are given on Ibex website).
 
 
 ### Building from sources ###
