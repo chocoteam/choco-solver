75ca1857815bdd37f0182cc9480f129e94cba732
Arnaud Malapert <arnaud.malapert@unice.fr>
2017-10-30 21:52:04 +0100

    Add new ternary distance constraints for relation (=, >, >=, <, <=) #150

---
+++ src/main/java/org/chocosolver/solver/constraints/ternary/PropLTDistanceXYZ.java
@@ -0,0 +1,52 @@
+/**
+ * This file is part of choco-solver, http://choco-solver.org/
+ *
+ * Copyright (c) 2017, IMT Atlantique. All rights reserved.
+ *
+ * Licensed under the BSD 4-clause license.
+ * See LICENSE file in the project root for full license information.
+ */
+package org.chocosolver.solver.constraints.ternary;
+
+import org.chocosolver.solver.constraints.Operator;
+import org.chocosolver.solver.exception.ContradictionException;
+import org.chocosolver.solver.variables.IntVar;
+
+/**
+ * @author Arnaud Malapert
+ * @since 10/30/2017
+ */
+public final class PropLTDistanceXYZ extends AbstractPropDistanceXYZ {
+
+	
+	public PropLTDistanceXYZ(IntVar[] vars) {
+		super(vars, Operator.LT);
+	}
+	
+	@Override
+	public void propagate(int evtmask) throws ContradictionException {
+		vars[Z].updateLowerBound(1, this);
+		propagate();
+	}
+	
+	@Override
+	protected boolean filterFromYZToX() throws ContradictionException {
+		return filterLowerFromIZToJ(Y, X, 1);
+	}
+
+	@Override
+	protected boolean filterFromXZToY() throws ContradictionException {
+		return filterLowerFromIZToJ(X, Y, 1);
+	}
+
+	@Override
+	protected boolean filterFromXYtoZ() throws ContradictionException {
+		return filterFromXYtoLBZ(1);
+	}
+
+	@Override
+	protected boolean isEntailed(int distance, int value) {
+		return distance < value;
+	}
+
+}
