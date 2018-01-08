e365f337019ffeac13d8038797ca52acf923d32c
cprudhom <cprudhom@mines-nantes.fr>
2018-01-08 18:24:36 +0100

    Update ibex release script

--- .travis/install-ibex.sh
+++ .travis/install-ibex.sh
@@ -3,13 +3,13 @@
 set -ex
 # download Ibex and untar it
 ibexver=2.6.3
-curl https://github.com/ibex-team/ibex-lib/archive/ibex-${ibexver}.tgz > ibex-${ibexver}.tar.gz
+curl https://codeload.github.com/ibex-team/ibex-lib/tar.gz/ibex-${ibexver} > ibex-${ibexver}.tar.gz
 tar -xzf ibex-${ibexver}.tar.gz
 # download java plugin
 curl http://www.ibex-lib.org/sites/default/files/ibex-java.tar.gz > ibex-java.tar.gz
-tar -xzf ibex-java.tar.gz -C ibex-${ibexver}/plugins
+tar -xzf ibex-java.tar.gz -C ibex-lib-ibex-${ibexver}/plugins
 # prepare installation
-cd ibex-${ibexver}
+cd ibex-lib-ibex-${ibexver}
 ./waf configure --enable-shared --with-jni --java-package-name=org.chocosolver.solver.constraints.real
 sudo ./waf install
 export LD_LIBRARY_PATH=/usr/local/lib
@@ -17,6 +17,6 @@ export LD_LIBRARY_PATH=/usr/local/lib
 #java -Djava.library.path=/usr/local/lib Test
 cd -
 sudo rm -r ibex-${ibexver}.tgz
-sudo rm -r ibex-${ibexver}
+sudo rm -r ibex-lib-ibex-${ibexver}
 sudo rm -r ibex-java.tar.gz
 
