465d76f97bc92567b9e57feca6010b51822178bd
cprudhom <cprudhom@mines-nantes.fr>
2018-07-20 10:19:57 +0200

    Rollback deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,21 +1,13 @@
 #!/usr/bin/env bash
 # based on: https://dracoblue.net/dev/uploading-snapshots-and-releases-to-maven-central-with-travis/
 
-function getVersionToRelease() {
-    CURRENT_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v "\[INFO\]"`
-    echo ${CURRENT_VERSION}
-}
-
-
-VERSION=$(getVersionToRelease)
-
-if [[ ${VERSION} = *"SNAPSHOT"* ]]
+if [ ! -z "$TRAVIS_TAG" ]
 then
-    echo "not on a tag -> keep snapshot version in pom.xml"
-else
-    echo "on a tag -> set pom.xml <version> to ${VERSION}"
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=${VERSION} 1>/dev/null 2>/dev/null
+    echo "on a tag -> set pom.xml <version> to ${TRAVIS_TAG}"
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=${TRAVIS_TAG} 1>/dev/null 2>/dev/null
     SETTINGS=" javadoc:jar source:jar"
+else
+    echo "not on a tag -> keep snapshot version in pom.xml"
 fi
 
 mvn -s .travis/settings.xml -P release ${SETTINGS} deploy -DskipTests -B -U -q
