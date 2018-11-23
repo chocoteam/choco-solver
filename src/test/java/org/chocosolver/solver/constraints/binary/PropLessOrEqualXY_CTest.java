91c01aa586b730f081cf023fa0e69f5d7a573b9d
cprudhom <cprudhom@mines-nantes.fr>
2018-11-23 11:38:54 +0100

    Update license header

--- src/test/java/org/chocosolver/solver/constraints/binary/PropLessOrEqualXY_CTest.java
+++ src/test/java/org/chocosolver/solver/constraints/binary/PropLessOrEqualXY_CTest.java
@@ -1,3 +1,11 @@
+/**
+ * This file is part of choco-solver, http://choco-solver.org/
+ *
+ * Copyright (c) 2018, IMT Atlantique. All rights reserved.
+ *
+ * Licensed under the BSD 4-clause license.
+ * See LICENSE file in the project root for full license information.
+ */
 package org.chocosolver.solver.constraints.binary;
 
 import org.chocosolver.solver.Model;
@@ -13,7 +21,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.mockito.ArgumentMatchers.eq;
-import static org.testng.Assert.*;
 
 /**
  * <p>
--- src/test/java/org/chocosolver/solver/constraints/nary/alldifferent/ExplAllDiffInstTest.java
+++ src/test/java/org/chocosolver/solver/constraints/nary/alldifferent/ExplAllDiffInstTest.java
@@ -1,3 +1,11 @@
+/**
+ * This file is part of choco-solver, http://choco-solver.org/
+ *
+ * Copyright (c) 2018, IMT Atlantique. All rights reserved.
+ *
+ * Licensed under the BSD 4-clause license.
+ * See LICENSE file in the project root for full license information.
+ */
 package org.chocosolver.solver.constraints.nary.alldifferent;
 
 import org.chocosolver.solver.Cause;
