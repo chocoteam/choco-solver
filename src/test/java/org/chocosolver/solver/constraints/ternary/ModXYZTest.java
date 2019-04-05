d622318519990905ddbb3c866b294464d91828ea
Arthur Godet <arth.godet@gmail.com>
2019-04-05 15:10:22 +0200

    simplify some tests + improve code coverage for PropModXYZ

--- src/test/java/org/chocosolver/solver/constraints/ternary/ModXYZTest.java
+++ src/test/java/org/chocosolver/solver/constraints/ternary/ModXYZTest.java
@@ -11,7 +11,7 @@ package org.chocosolver.solver.constraints.ternary;
 
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.constraints.Constraint;
-import org.chocosolver.solver.constraints.extension.nary.PropLargeGACSTRPos;
+import org.chocosolver.solver.constraints.ConstraintsName;
 import org.chocosolver.solver.exception.ContradictionException;
 import org.chocosolver.solver.exception.SolverException;
 import org.chocosolver.solver.variables.IntVar;
@@ -32,7 +32,7 @@ public class ModXYZTest extends AbstractTernaryTest {
 
 	@Override
 	protected int validTuple(int vx, int vy, int vz) {
-		return (vy != 0 && vz == vx - vy * (vx / vy)) ? 1 : 0;
+		return (vy != 0 && vz == vx%vy) ? 1 : 0;
 	}
 
 	@Override
@@ -87,6 +87,61 @@ public class ModXYZTest extends AbstractTernaryTest {
 		}
 	}
 
+	@Test(groups="1s", timeOut=60000)
+	public void testMod3VarNegValues2() throws ContradictionException {
+		Model model = new Model("model");
+		IntVar x = model.intVar("x", -5, 0);
+		IntVar y = model.intVar("y", -3, 3);
+		IntVar z = model.intVar("z", -5,5);
+		model.mod(x, y, z).post();
+		model.getSolver().propagate();
+		Assert.assertEquals(z.getDomainSize(), 3);
+		Assert.assertEquals(x.getDomainSize(), 6);
+		Assert.assertEquals(y.getDomainSize(), 6);
+		for(int i = -5; i<=0; i++) {
+			Assert.assertTrue(x.contains(i));
+		}
+		for(int j = -2; j<=0; j++) {
+			Assert.assertTrue(z.contains(j));
+		}
+		for(int k = -3; k<=3; k++) {
+			if(k == 0) {
+				Assert.assertFalse(y.contains(k));
+			} else {
+				Assert.assertTrue(y.contains(k));
+			}
+		}
+	}
+
+	@Test(groups="1s", timeOut=60000)
+	public void testMod3VarNegValues3() throws ContradictionException {
+		Model model = new Model("model");
+		IntVar x = model.intVar("x", -5, 5);
+		IntVar y = model.intVar("y", new int[]{-3, -2, 0, 2, 3});
+		IntVar z = model.intVar("z", -5,0);
+		model.mod(x, y, z).post();
+		model.getSolver().propagate();
+		Assert.assertEquals(z.getDomainSize(), 3);
+		Assert.assertEquals(x.getDomainSize(), 9);
+		Assert.assertEquals(y.getDomainSize(), 4);
+		for(int i = -5; i<=0; i++) {
+			Assert.assertTrue(x.contains(i));
+		}
+		Assert.assertTrue(x.contains(2));
+		Assert.assertTrue(x.contains(3));
+		Assert.assertTrue(x.contains(4));
+		for(int j = -2; j<=0; j++) {
+			Assert.assertTrue(z.contains(j));
+		}
+		for(int k = -3; k<=3; k++) {
+			if(k==0 || k==-1 || k==1) {
+				Assert.assertFalse(y.contains(k));
+			} else {
+				Assert.assertTrue(y.contains(k));
+			}
+		}
+	}
+
 	@Test(groups="1s", timeOut=60000, expectedExceptions = SolverException.class)
 	public void testMod3VarsZeroDiv() {
 		Model model = new Model("model");
@@ -115,9 +170,7 @@ public class ModXYZTest extends AbstractTernaryTest {
 		model.mod(x, y, z).post();
 		Assert.assertEquals(model.getNbCstrs(), 1);
 		Constraint constraint = model.getCstrs()[0];
-		Assert.assertEquals(constraint.getPropagators().length, 1);
-		System.out.println(constraint.getPropagators()[0].getClass());
-		Assert.assertTrue(constraint.getPropagators()[0].getClass() == PropLargeGACSTRPos.class);
+		Assert.assertTrue(constraint.getName().equals(ConstraintsName.TABLE));
 	}
 
 	@Test(groups="1s", timeOut=60000)
@@ -131,5 +184,20 @@ public class ModXYZTest extends AbstractTernaryTest {
 		Constraint constraint = model.getCstrs()[0];
 		Assert.assertEquals(constraint.getPropagators().length, 1);
 		Assert.assertTrue(constraint.getPropagators()[0].getClass() == PropModXYZ.class);
+	}
+
+	@Test(groups="1s", timeOut=60000)
+	public void testMod3VarsBoundedPropag() throws ContradictionException {
+		Model model = new Model("model");
+		IntVar x = model.intVar("x", 0,100_000);
+		IntVar y = model.intVar("y", 0, 9);
+		IntVar z = model.intVar("z", -10, 10);
+		model.mod(x, y, z).post();
+		model.getSolver().propagate();
+
+		Assert.assertTrue(z.getLB() == 0);
+		Assert.assertTrue(z.getUB() == 8);
+
+		Assert.assertTrue(y.getLB() == 1);
 	}
 }
