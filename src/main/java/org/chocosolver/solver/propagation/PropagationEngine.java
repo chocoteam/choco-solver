bed19d08b6c496ed53ae0f99751ee91d061c4f3f
cprudhom <cprudhom@mines-nantes.fr>
2019-07-18 16:38:38 +0200

    Update test wrt last commit

--- src/main/java/org/chocosolver/solver/propagation/PropagationEngine.java
+++ src/main/java/org/chocosolver/solver/propagation/PropagationEngine.java
@@ -95,6 +95,7 @@ public class PropagationEngine {
      * Each of the seven queues deals with on priority.
      * When a propagator needs to be executed, it is scheduled in the queue corresponding to its priority.
      * The lowest priority queue is emptied before one element of the second lowest queue is popped, etc.
+     *
      * @param model the declaring model
      */
     public PropagationEngine(Model model) {
@@ -115,6 +116,7 @@ public class PropagationEngine {
      * Build up internal structure, if not yet done, in order to allow propagation.
      * If new constraints are added after having initializing the engine, dynamic addition is used.
      * A call to clear erase the internal structure, and allow new initialisation.
+     *
      * @throws SolverException if a constraint is declared more than once in this propagation engine
      */
     public void initialize() throws SolverException {
@@ -155,7 +157,7 @@ public class PropagationEngine {
     /**
      * Launch the proapagation, ie, active propagators if necessary, then reach a fix point
      *
-     * @throws ContradictionException if a contradiction occurrs
+     * @throws ContradictionException if a contradiction occurs
      */
     @SuppressWarnings({"NullableProblems"})
     public void propagate() throws ContradictionException {
@@ -202,6 +204,7 @@ public class PropagationEngine {
     /**
      * Execute 'coarse' propagation on a newly added propagator
      * or one that should be propagated on backtrack
+     *
      * @param propagator a propagator to propagate
      * @throws ContradictionException if propagation fails
      */
@@ -226,7 +229,7 @@ public class PropagationEngine {
     }
 
     private int nextNotEmpty() {
-        if(notEmpty == 0)return -1;
+        if (notEmpty == 0) return -1;
         return Integer.numberOfTrailingZeros(notEmpty);
     }
 
@@ -255,7 +258,7 @@ public class PropagationEngine {
      *
      * @param variable modified variable
      * @param type     type of modification event
-     * @param cause origin of the modification
+     * @param cause    origin of the modification
      */
     public void onVariableUpdate(Variable variable, IEventType type, ICause cause) {
         if (CHECK_SCOPE && Propagator.class.isAssignableFrom(cause.getClass())) {
@@ -302,8 +305,9 @@ public class PropagationEngine {
 
     /**
      * Exeucte a delayed propagator
+     *
      * @param propagator propagator to execute
-     * @param type type of event to execute
+     * @param type       type of event to execute
      */
     public void delayedPropagation(Propagator propagator, PropagatorEventType type) {
         assert propagator == lastProp;
@@ -313,6 +317,7 @@ public class PropagationEngine {
 
     /**
      * Action to do when a propagator is executed
+     *
      * @param propagator propagator to execute
      */
     public void onPropagatorExecution(Propagator propagator) {
@@ -361,7 +366,7 @@ public class PropagationEngine {
      *
      * @param permanent does the constraint is permanently added
      * @param ps        propagators to add
-     * * @throws SolverException if a constraint is declared more than once in this propagation engine
+     *                  * @throws SolverException if a constraint is declared more than once in this propagation engine
      */
     public void dynamicAddition(boolean permanent, Propagator... ps) throws SolverException {
         int nbp = ps.length;
@@ -478,8 +483,8 @@ public class PropagationEngine {
             }
         }
 
-        private void removeAt(int p){
-            if(p < size - 1) {
+        private void removeAt(int p) {
+            if (p < size - 1) {
                 System.arraycopy(elements, p + 1, elements, p, size - p);
                 System.arraycopy(keys, p + 1, keys, p, size - p);
             }
