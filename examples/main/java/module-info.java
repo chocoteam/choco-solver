4ea84d2da44e9e85b45a018ba78bb9a670048659
cprudhom <charles.prudhomme@imt-atlantique.fr>
2019-11-04 10:43:58 +0100

    Move examples into choco (maven part)

---
+++ examples/main/java/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * <br/>
+ *
+ * @author Charles Prud'homme
+ * @since 31/10/2019
+ */
+module org.chocosolver.examples {
+    requires org.chocosolver.solver;
+    requires org.chocosolver.pf4cs;
+    requires args4j;
+    requires java.desktop;
+}
