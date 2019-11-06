fb52803693b65f499532c98a9f902920f711bb38
cprudhom <charles.prudhomme@imt-atlantique.fr>
2019-11-06 15:47:27 +0100

    To dbg ibex install

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -6,7 +6,7 @@ if [ "${TEST_SUITE}" == "ibex" ]
 then
   curl https://raw.githubusercontent.com/sormuras/bach/master/install-jdk.sh > install-jdk.sh
   export JAVA_HOME=$HOME/openjdk9
-  /bin/bash $TRAVIS_BUILD_DIR/install-jdk.sh -f 9 --target $JAVA_HOME
+  /bin/bash $TRAVIS_BUILD_DIR/install-jdk.sh -f 9 -t "$JAVA_HOME"
 
   set -ex
   # download Ibex and untar it
@@ -25,8 +25,9 @@ then
   export LD_LIBRARY_PATH=/usr/local/lib
 
   export JAVA_HOME=$HOME/openjdk11
-  /bin/bash $TRAVIS_BUILD_DIR/install-jdk.sh -f 11 --target $JAVA_HOME
+  /bin/bash $TRAVIS_BUILD_DIR/install-jdk.sh -f 11 -t "$JAVA_HOME" -v
 
+  echo "$JAVA_HOME"
   mvn -v
 
   mvn validate -Dibex.path=${TRAVIS_BUILD_DIR}/ibex/plugins/java
