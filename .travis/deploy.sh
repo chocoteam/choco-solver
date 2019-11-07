b6e19397c2b39f80b8de79f6a71ad8297d8e8fb8
cprudhom <charles.prudhomme@imt-atlantique.fr>
2019-11-07 15:48:12 +0100

    last config of .travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,7 +5,7 @@ if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to ${TRAVIS_TAG}"
     mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=${TRAVIS_TAG}
-    SETTINGS=" javadoc:jar source:jar"
+    export SETTINGS=" javadoc:jar source:jar"
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
 fi
