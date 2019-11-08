71e2963ebe0fd85595397b2ea5d41b02e5e83e34
cprudhom <charles.prudhomme@imt-atlantique.fr>
2019-11-08 18:00:32 +0100

    Fix deploy gpg issue

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # based on: https://dracoblue.net/dev/uploading-snapshots-and-releases-to-maven-central-with-travis/
 
+echo $GPG_SECRET_KEYS | base64 --decode | gpg --import
+echo $GPG_OWNERTRUST | base64 --decode | gpg --import-ownertrust
+
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to ${TRAVIS_TAG}"
