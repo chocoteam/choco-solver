32e434d22142354df26b8e46df5f5fbf80288938
cprudhom <charles.prudhomme@imt-atlantique.fr>
2019-11-08 16:06:04 +0100

    Update deploy.sh to fix wrong path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,10 @@
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to ${TRAVIS_TAG}"
-    mvn --settings scripts/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=${TRAVIS_TAG}
+    mvn -s scripts/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=${TRAVIS_TAG}
     export SETTINGS=" javadoc:jar source:jar"
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
 fi
 
-mvn -s .travis/settings.xml -P release ${SETTINGS} deploy -DskipTests -B -U -q
+mvn -s scripts/settings.xml -P release ${SETTINGS} deploy -DskipTests -B -U -q
