12afdbe790c7ce740d3ee7376e61a610d9def285
cprudhom <charles.prudhomme@imt-atlantique.fr>
2020-03-18 17:04:20 +0100

    Add ConflictHistorySearch blackbox heuristic

---
+++ solver/src/main/java/org/chocosolver/solver/search/strategy/selectors/variables/AbsctractCriterionBasedStrategy.java
@@ -0,0 +1,108 @@
+/*
+ * This file is part of choco-solver, http://choco-solver.org/
+ *
+ * Copyright (c) 2019, IMT Atlantique. All rights reserved.
+ *
+ * Licensed under the BSD 4-clause license.
+ *
+ * See LICENSE file in the project root for full license information.
+ */
+package org.chocosolver.solver.search.strategy.selectors.variables;
+
+import gnu.trove.list.array.TIntArrayList;
+import org.chocosolver.memory.IStateInt;
+import org.chocosolver.solver.search.strategy.assignments.DecisionOperatorFactory;
+import org.chocosolver.solver.search.strategy.decision.Decision;
+import org.chocosolver.solver.search.strategy.selectors.values.IntValueSelector;
+import org.chocosolver.solver.search.strategy.strategy.AbstractStrategy;
+import org.chocosolver.solver.variables.IntVar;
+import org.chocosolver.util.objects.IntMap;
+
+/**
+ * <p>
+ * Project: choco.
+ *
+ * @author Charles Prud'homme
+ * @since 26/02/2020.
+ */
+public abstract class AbsctractCriterionBasedStrategy extends AbstractStrategy<IntVar> {
+
+    /**
+     * Randomness to break ties
+     */
+    private java.util.Random random;
+    /***
+     * Pointer to the last uninstantiated variable
+     */
+    private IStateInt last;
+    /**
+     * /** The way value is selected for a given variable
+     */
+    private IntValueSelector valueSelector;
+    /**
+     * Temporary. Stores index of variables with the same (best) score
+     */
+    private TIntArrayList bests = new TIntArrayList();
+
+    /**
+     * Kind of duplicate of pid2ari to limit calls of backtrackable objects
+     */
+    IntMap pid2arity;
+
+    public AbsctractCriterionBasedStrategy(IntVar[] vars, long seed,
+        IntValueSelector valueSelector) {
+        super(vars);
+        this.random = new java.util.Random(seed);
+        this.valueSelector = valueSelector;
+        this.last = vars[0].getModel().getEnvironment().makeInt(vars.length - 1);
+        pid2arity = new IntMap(vars[0].getModel().getCstrs().length * 3 / 2 + 1, -1);
+    }
+
+    @Override
+    public Decision<IntVar> getDecision() {
+        IntVar best = null;
+        bests.resetQuick();
+        pid2arity.clear();
+        double w = 0.;
+        int to = last.get();
+        for (int idx = 0; idx <= to; idx++) {
+            int dsize = vars[idx].getDomainSize();
+            if (dsize > 1) {
+                double weight = weight(vars[idx]);
+                if (w < weight) {
+                    bests.resetQuick();
+                    bests.add(idx);
+                    w = weight;
+                } else if (w == weight) {
+                    bests.add(idx);
+                }
+            } else {
+                // swap
+                IntVar tmp = vars[to];
+                vars[to] = vars[idx];
+                vars[idx] = tmp;
+                idx--;
+                to--;
+            }
+        }
+        last.set(to);
+        if (bests.size() > 0) {
+            int currentVar = bests.get(random.nextInt(bests.size()));
+            best = vars[currentVar];
+        }
+        return computeDecision(best);
+    }
+
+    @Override
+    public Decision<IntVar> computeDecision(IntVar variable) {
+        if (variable == null || variable.isInstantiated()) {
+            return null;
+        }
+        int currentVal = valueSelector.selectValue(variable);
+        return variable.getModel().getSolver().getDecisionPath()
+            .makeIntDecision(variable, DecisionOperatorFactory
+                .makeIntEq(), currentVal);
+    }
+
+    protected abstract double weight(IntVar v);
+}
