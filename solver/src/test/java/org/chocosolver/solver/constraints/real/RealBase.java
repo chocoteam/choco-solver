401b3bcffa34265a8a1373f87ffb9cdd8f4db0b9
João Pedro Schmitt <schmittjoaopedro@gmail.com>
2020-09-16 22:12:28 -0300

    fix math expressions that call ibex to compute the bounds fix bounds calculation for negative power with constant exponent

---
+++ solver/src/test/java/org/chocosolver/solver/constraints/real/RealBase.java
@@ -0,0 +1,63 @@
+/*
+ * This file is part of choco-solver, http://choco-solver.org/
+ *
+ * Copyright (c) 2020, IMT Atlantique. All rights reserved.
+ *
+ * Licensed under the BSD 4-clause license.
+ *
+ * See LICENSE file in the project root for full license information.
+ */
+package org.chocosolver.solver.constraints.real;
+
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.expression.continuous.relational.CReExpression;
+import org.chocosolver.solver.expression.discrete.relational.ReExpression;
+import org.chocosolver.solver.variables.BoolVar;
+import org.chocosolver.solver.variables.RealVar;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class RealBase {
+
+    protected static final double MIN_LIM = -100.0;
+    protected static final double MAX_LIM = 100.0;
+    protected static final double PRECISION = 0.000_001;
+
+    protected Model model;
+
+    protected RealVar x;
+
+    protected RealVar y;
+
+    protected RealVar z;
+
+    protected RealVar v;
+
+    protected RealVar r;
+
+    protected BoolVar w;
+
+    @BeforeMethod
+    protected void before() {
+        model = new Model();
+        x = model.realVar("x", MIN_LIM, MAX_LIM, PRECISION);
+        y = model.realVar("y", MIN_LIM, MAX_LIM, PRECISION);
+        z = model.realVar("z", MIN_LIM, MAX_LIM, PRECISION);
+        v = model.realVar("v", MIN_LIM, MAX_LIM, PRECISION);
+        r = model.realVar("r", 0.0, MAX_LIM, PRECISION);
+        w = model.boolVar("w");
+    }
+
+    protected void assertBound(RealVar var, double lb, double ub) {
+        Assert.assertEquals(var.getLB(), lb, PRECISION);
+        Assert.assertEquals(var.getUB(), ub, PRECISION);
+    }
+
+    protected void postExpression(CReExpression expression) {
+        expression.ibex(PRECISION).post();
+    }
+
+    protected ReExpression getExpression(CReExpression expression) {
+        return expression.ibex(PRECISION).reify().eq(1);
+    }
+}
---
+++ solver/src/test/java/org/chocosolver/solver/constraints/real/RealNegativePowerTest.java
@@ -0,0 +1,132 @@
+/*
+ * This file is part of choco-solver, http://choco-solver.org/
+ *
+ * Copyright (c) 2020, IMT Atlantique. All rights reserved.
+ *
+ * Licensed under the BSD 4-clause license.
+ *
+ * See LICENSE file in the project root for full license information.
+ */
+package org.chocosolver.solver.constraints.real;
+
+import org.chocosolver.solver.constraints.real.RealBase;
+import org.chocosolver.solver.exception.ContradictionException;
+import org.testng.annotations.Test;
+
+public class RealNegativePowerTest extends RealBase {
+
+    /**
+     * With even operations
+     */
+
+    @Test
+    public void powerEven1Test() throws ContradictionException {
+        postExpression(y.eq(x.pow(-2)));
+        model.getSolver().propagate();
+        assertBound(x, -100.0, 100.0);
+        assertBound(y, 0.0001, 100.0);
+    }
+
+    @Test
+    public void powerEven2Test() throws ContradictionException {
+        postExpression(x.ge(-0.5));
+        postExpression(x.le(0.5));
+        postExpression(y.eq(x.pow(-2)));
+        model.getSolver().propagate();
+        assertBound(x, -0.5, 0.5);
+        assertBound(y, 4.0, 100.0);
+    }
+
+    @Test
+    public void powerEven3Test() throws ContradictionException {
+        postExpression(x.ge(0));
+        postExpression(x.le(0.5));
+        postExpression(y.eq(x.pow(-2)));
+        model.getSolver().propagate();
+        assertBound(x, 0.1, 0.5);
+        assertBound(y, 4.0, 100.0);
+    }
+
+    @Test
+    public void powerEven4Test() throws ContradictionException {
+        postExpression(x.ge(2.0));
+        postExpression(y.eq(x.pow(-2)));
+        model.getSolver().propagate();
+        assertBound(x, 2.0, 100.0);
+        assertBound(y, 0.0001, 0.25);
+    }
+
+    @Test
+    public void powerEven5Test() throws ContradictionException {
+        postExpression(x.ge(-0.5));
+        postExpression(x.le(0));
+        postExpression(y.eq(x.pow(-2)));
+        model.getSolver().propagate();
+        assertBound(x, -0.5, -0.1);
+        assertBound(y, 4.0, 100.0);
+    }
+
+    @Test
+    public void powerEven6Test() throws ContradictionException {
+        postExpression(x.ge(-10000));
+        postExpression(x.le(-2));
+        postExpression(y.eq(x.pow(-2)));
+        model.getSolver().propagate();
+        assertBound(x, -100.0, -2.0);
+        assertBound(y, 0.0001, 0.25);
+    }
+
+    /**
+     * With odd operations
+     */
+
+    @Test
+    public void powerOdd2Test() throws ContradictionException {
+        postExpression(x.ge(-2));
+        postExpression(x.le(2));
+        postExpression(y.eq(x.pow(-3)));
+        model.getSolver().propagate();
+        assertBound(x, -2.0, 2.0);
+        assertBound(y, -100.0, 100.0);
+    }
+
+    @Test
+    public void powerOdd3Test() throws ContradictionException {
+        postExpression(x.ge(0));
+        postExpression(x.le(2));
+        postExpression(y.eq(x.pow(-3)));
+        model.getSolver().propagate();
+        assertBound(x, 0.215443, 2.0);
+        assertBound(y, 0.125, 100.0);
+    }
+
+    @Test
+    public void powerOdd4Test() throws ContradictionException {
+        postExpression(x.ge(2));
+        postExpression(y.eq(x.pow(-3)));
+        model.getSolver().propagate();
+        assertBound(x, 2.0, 100.0);
+        assertBound(y, 0.000001, 0.125);
+    }
+
+    @Test
+    public void powerOdd5Test() throws ContradictionException {
+        postExpression(x.ge(-2));
+        postExpression(x.le(0));
+        postExpression(y.eq(x.pow(-3)));
+        model.getSolver().propagate();
+        assertBound(x, -2.0, -0.215443);
+        assertBound(y, -100.0, -0.125);
+    }
+
+    @Test
+    public void powerOdd6Test() throws ContradictionException {
+        postExpression(x.ge(-10000.0));
+        postExpression(x.le(-2));
+        postExpression(y.eq(x.pow(-3)));
+        model.getSolver().propagate();
+        assertBound(x, -100.0, -2.0);
+        assertBound(y, -0.125, -0.000001);
+    }
+
+}
