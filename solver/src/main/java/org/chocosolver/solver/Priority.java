378fb62805bdafd25966c752c0086b28bf9fc8b6
Arthur Godet <arth.godet@gmail.com>
2020-12-06 15:52:59 +0100

    added Choco licence in files' head when maven compiling

--- solver/src/main/java/org/chocosolver/solver/Priority.java
+++ solver/src/main/java/org/chocosolver/solver/Priority.java
@@ -1,4 +1,13 @@
 /*
+ * This file is part of choco-solver, http://choco-solver.org/
+ *
+ * Copyright (c) 2020, IMT Atlantique. All rights reserved.
+ *
+ * Licensed under the BSD 4-clause license.
+ *
+ * See LICENSE file in the project root for full license information.
+ */
+/*
 @author Arthur Godet <arth.godet@gmail.com>
 @since 04/12/2020
 */
