a28229430649c527e7c0cc93813002b9585f8d69
Dimitri <dimitri.justeau@gmail.com>
2021-03-22 17:09:10 +0100

    Add tests for tree, forest, directedTree, and directedForest graph constraints. Only use degree propagators in directedTree and directedForest constraints as PropArborescences currently filters too much solutions

--- solver/src/test/java/org/chocosolver/solver/constraints/graph/tree/DirectedTreeForestTest.java
+++ solver/src/test/java/org/chocosolver/solver/constraints/graph/tree/DirectedTreeForestTest.java
@@ -11,16 +11,20 @@ package org.chocosolver.solver.constraints.graph.tree;
 
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.constraints.Constraint;
 import org.chocosolver.solver.search.strategy.strategy.GraphStrategy;
 import org.chocosolver.solver.variables.DirectedGraphVar;
+import org.chocosolver.util.ESat;
 import org.chocosolver.util.objects.graphs.DirectedGraph;
+import org.chocosolver.util.objects.graphs.GraphFactory;
 import org.chocosolver.util.objects.setDataStructures.SetType;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-public class ArborescenceTest {
+public class DirectedTreeForestTest {
 
     private static SetType graphTypeEnv = SetType.BIPARTITESET;
     private static SetType graphTypeKer = SetType.BIPARTITESET;
@@ -93,5 +97,67 @@ public class ArborescenceTest {
             System.out.println("env:" + ge + " ker :" + ge);
             smallTrees();
         }
+    }
+
+    @Test(groups="10s", timeOut=60000)
+    public void directedTreeGenerateTest() {
+        // Generate solutions with filtering
+        Model model = new Model();
+        int n = 5;
+        DirectedGraph LB = GraphFactory.makeStoredDirectedGraph(model, n, SetType.BITSET, SetType.BITSET);
+        DirectedGraph UB = GraphFactory.makeCompleteStoredDirectedGraph(model, n, SetType.BITSET, SetType.BITSET, false);
+        for (int i = 1; i < n; i++) {
+            UB.removeEdge(0, i);
+        }
+        DirectedGraphVar g = model.digraphVar("g", LB, UB);
+        model.directedTree(g, 0).post();
+        while (model.getSolver().solve()) {}
+        // Generate solutions with checker
+        Model model2 = new Model();
+        DirectedGraph LB2 = GraphFactory.makeStoredDirectedGraph(model2, n, SetType.BITSET, SetType.BITSET);
+        DirectedGraph UB2 = GraphFactory.makeCompleteStoredDirectedGraph(model2, n, SetType.BITSET, SetType.BITSET, false);
+        for (int i = 1; i < n; i++) {
+            UB2.removeEdge(0, i);
+        }
+        DirectedGraphVar g2 = model2.digraphVar("g", LB2, UB2);
+        Constraint cons = model2.directedTree(g2, 0);
+        int count = 0;
+        while (model2.getSolver().solve()) {
+            if (cons.isSatisfied() == ESat.TRUE) {
+                count++;
+            }
+        }
+        Assert.assertEquals(model.getSolver().getSolutionCount(), count);
+    }
+
+    @Test(groups="10s", timeOut=60000)
+    public void directedForestGenerateTest() {
+        // Generate solutions with filtering
+        Model model = new Model();
+        int n = 5;
+        DirectedGraph LB = GraphFactory.makeStoredDirectedGraph(model, n, SetType.BITSET, SetType.BITSET);
+        DirectedGraph UB = GraphFactory.makeCompleteStoredDirectedGraph(model, n, SetType.BITSET, SetType.BITSET, false);
+        for (int i = 2; i < n; i++) {
+            UB.removeEdge(0, i);
+        }
+        DirectedGraphVar g = model.digraphVar("g", LB, UB);
+        model.directedForest(g).post();
+        while (model.getSolver().solve()) {}
+        // Generate solutions with checker
+        Model model2 = new Model();
+        DirectedGraph LB2 = GraphFactory.makeStoredDirectedGraph(model2, n, SetType.BITSET, SetType.BITSET);
+        DirectedGraph UB2 = GraphFactory.makeCompleteStoredDirectedGraph(model2, n, SetType.BITSET, SetType.BITSET, false);
+        for (int i = 2; i < n; i++) {
+            UB2.removeEdge(0, i);
+        }
+        DirectedGraphVar g2 = model2.digraphVar("g", LB2, UB2);
+        Constraint cons = model2.directedForest(g2);
+        int count = 0;
+        while (model2.getSolver().solve()) {
+            if (cons.isSatisfied() == ESat.TRUE) {
+                count++;
+            }
+        }
+        Assert.assertEquals(model.getSolver().getSolutionCount(), count);
     }
 }
