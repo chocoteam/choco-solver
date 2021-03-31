cbf06818a635f056ce9da214929ac9f661646a7c
Dimitri <dimitri.justeau@gmail.com>
2021-03-31 15:32:20 +0200

    Implement and test SetDifference

---
+++ solver/src/main/java/org/chocosolver/util/objects/setDataStructures/SetDifference.java
@@ -0,0 +1,142 @@
+/*
+ * This file is part of choco-solver, http://choco-solver.org/
+ *
+ * Copyright (c) 2021, IMT Atlantique. All rights reserved.
+ *
+ * Licensed under the BSD 4-clause license.
+ *
+ * See LICENSE file in the project root for full license information.
+ */
+package org.chocosolver.util.objects.setDataStructures;
+
+import org.chocosolver.solver.Model;
+
+/**
+ * Set representing the difference between two sets (setA \ setB).
+ * Constructed incrementally when observed sets are modified.
+ * This set is read-only.
+ *
+ * @author Dimitri Justeau-Allaire
+ * @since 31/03/2021
+ */
+public class SetDifference extends AbstractSet {
+
+    public ISet setA;
+    public ISet setB;
+    protected ISet values;
+
+    public SetDifference(ISet setA, ISet setB) {
+        this.setA = setA;
+        this.setB = setB;
+        this.values = SetFactory.makeRangeSet();
+        init();
+    }
+
+    public SetDifference(Model model, ISet setA, ISet setB) {
+        this.setA = setA;
+        this.setB = setB;
+        this.values = SetFactory.makeStoredSet(SetType.RANGESET, 0, model);
+        init();
+    }
+
+    private void init() {
+        setA.registerObserver(this, 0);
+        setB.registerObserver(this, 1);
+        // init the difference
+        for (int v : setA) {
+            if (!setB.contains(v)) {
+                values.add(v);
+            }
+        }
+    }
+
+    @Override
+    public ISetIterator iterator() {
+        return values.iterator();
+    }
+
+    @Override
+    public ISetIterator newIterator() {
+        return values.newIterator();
+    }
+
+    @Override
+    public boolean add(int element) {
+        throw new UnsupportedOperationException("this set is read-only");
+    }
+
+    @Override
+    public boolean remove(int element) {
+        throw new UnsupportedOperationException("this set is read-only");
+    }
+
+
+    @Override
+    public boolean contains(int element) {
+        return values.contains(element);
+    }
+
+    @Override
+    public int size() {
+        return values.size();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("this set is read-only");
+    }
+
+    @Override
+    public int min() {
+        return values.min();
+    }
+
+    @Override
+    public int max() {
+        return values.max();
+    }
+
+    @Override
+    public SetType getSetType() {
+        return SetType.DYNAMIC;
+    }
+
+    @Override
+    public void notifyElementRemoved(int element, int idx) {
+        if (idx == 0) {
+            values.remove(element);
+            notifyObservingElementRemoved(element);
+        }
+        if (idx == 1 && setA.contains(element)) {
+            values.add(element);
+            notifyObservingElementAdded(element);
+        }
+    }
+
+    @Override
+    public void notifyElementAdded(int element, int idx) {
+        if (idx == 0 && !setB.contains(element)) {
+            values.add(element);
+            notifyObservingElementAdded(element);
+        }
+        if (idx == 1 && setA.contains(element)) {
+            values.remove(element);
+            notifyObservingElementRemoved(element);
+        }
+    }
+
+    @Override
+    public void notifyCleared(int idx) {
+        if (idx == 0) {
+            values.clear();
+            notifyObservingCleared();
+        }
+        if (idx == 0) {
+            for (int v : setA) {
+                if (values.add(v)) {
+                    notifyObservingElementAdded(v);
+                }
+            }
+        }
+    }
+}
---
+++ solver/src/test/java/org/chocosolver/util/objects/setDataStructures/nonbacktrackable/SetDifferenceTest.java
@@ -0,0 +1,70 @@
+/*
+ * This file is part of choco-solver, http://choco-solver.org/
+ *
+ * Copyright (c) 2021, IMT Atlantique. All rights reserved.
+ *
+ * Licensed under the BSD 4-clause license.
+ *
+ * See LICENSE file in the project root for full license information.
+ */
+package org.chocosolver.util.objects.setDataStructures.nonbacktrackable;
+
+import org.chocosolver.solver.Model;
+import org.chocosolver.util.objects.setDataStructures.ISet;
+import org.chocosolver.util.objects.setDataStructures.SetFactory;
+import org.chocosolver.util.objects.setDataStructures.SetDifference;
+import org.chocosolver.util.objects.setDataStructures.SetType;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+/**
+ * Tests for SetDifference class
+ * @author Dimitri Justeau-Allaire
+ * @since 31/03/2021
+ */
+public class SetDifferenceTest {
+
+    @Test(groups = "1s", timeOut=60000)
+    public void testSetDifference1() {
+        ISet setA = SetFactory.makeConstantSet(new int[] {0, 1, 2, 3, 4, 5});
+        ISet setB = SetFactory.makeConstantSet(new int[] {1, 3, 5});
+        ISet difference = new SetDifference(setA, setB);
+        Assert.assertEquals(difference.min(), 0);
+        Assert.assertEquals(difference.max(), 4);
+        Assert.assertEquals(difference.size(), 3);
+        int[] vals = difference.toArray();
+        Arrays.sort(vals);
+        Assert.assertTrue(Arrays.equals(vals, new int[] {0, 2, 4}));
+    }
+
+    @Test(groups = "1s", timeOut=60000)
+    public void testSetDifference2() {
+        Model m = new Model();
+        ISet setA = SetFactory.makeStoredSet(SetType.BITSET, 0, m);
+        ISet setB = SetFactory.makeStoredSet(SetType.BITSET, -2, m);
+        ISet difference = new SetDifference(m, setA, setB);
+        setA.add(0); setA.add(2); setA.add(4);
+        setB.add(-2); setB.add(-1); setB.add(4);
+        Assert.assertEquals(difference.size(), 2);
+        int[] vals = difference.toArray();
+        Arrays.sort(vals);
+        Assert.assertTrue(Arrays.equals(vals, new int[] {0, 2}));
+    }
+
+    @Test(groups = "1s", timeOut=60000)
+    public void testSetDifference3() {
+        ISet setA = SetFactory.makeBitSet(0);
+        ISet setB = SetFactory.makeBitSet(-2);
+        ISet setC = SetFactory.makeBipartiteSet(0);
+        ISet difference = new SetDifference(setA, setB);
+        ISet difference2 = new SetDifference(difference, setC);
+        setA.add(0); setA.add(2); setA.add(4); setA.add(6);
+        setB.add(-2); setB.add(-1); setB.add(6); setB.add(4); setB.add(0); setB.remove(0);
+        setC.add(0); setC.add(7);
+        int[] vals = difference2.toArray();
+        Arrays.sort(vals);
+        Assert.assertTrue(Arrays.equals(vals, new int[] {2}));
+    }
+}
