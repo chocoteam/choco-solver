f31a58deb855026abf7a9b80c49af22660491830
cprudhom <charles.prudhomme@imt-atlantique.fr>
2021-08-25 12:05:36 +0200

    Update test after #b118dd192bc5e15d228d2b7d25dd5dc93a2781e1

--- solver/src/test/java/org/chocosolver/solver/search/loop/SolverTest.java
+++ solver/src/test/java/org/chocosolver/solver/search/loop/SolverTest.java
@@ -112,7 +112,7 @@ public class SolverTest {
         r.setHBFS(.05, .1, 32);
         while (model.getSolver().solve()) ;
         assertEquals(model.getSolver().getSolutionCount(), 7);
-        assertEquals(model.getSolver().getNodeCount(), 5777);
+        assertEquals(model.getSolver().getNodeCount(), 4542);
     }
 
     @Test(groups = "1s", timeOut = 60000)
