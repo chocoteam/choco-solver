02f1ce0f6919f68dc3ea8da03794570df9320149
cprudhom <charles.prudhomme@imt-atlantique.fr>
2021-08-25 11:12:51 +0200

    Fix wrong code: - reason: '<>' with anonymous inner classes is not supported in -source 8

--- solver/src/main/java/org/chocosolver/solver/search/strategy/Search.java
+++ solver/src/main/java/org/chocosolver/solver/search/strategy/Search.java
@@ -631,7 +631,7 @@ public class Search {
      * @return a strategy that lets Ibex terminates the solving process.
      */
     public static AbstractStrategy ibexSolving(Model model) {
-        return new AbstractStrategy<>(model.getVars()) {
+        return new AbstractStrategy(model.getVars()) {
             final IbexDecision dec = new IbexDecision(model);
 
             @Override
