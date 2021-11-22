cfb3e584bce5bf488e5448d6c0d1a339c20d9669
cprudhom <charles.prudhomme@imt-atlantique.fr>
2021-11-22 11:30:21 +0100

    Add test (#848)

--- solver/src/test/java/org/chocosolver/solver/constraints/ternary/ExplPropXplusYeqZTest.java
+++ solver/src/test/java/org/chocosolver/solver/constraints/ternary/ExplPropXplusYeqZTest.java
@@ -216,4 +216,18 @@ public class ExplPropXplusYeqZTest {
             Assert.fail();
         }
     }
+
+    @Test(groups = "1s", timeOut = 60000)
+    public void testPropXplusYeqZFailure2() {
+        Model m = new Model();
+        IntVar a = m.intVar("a", 0, 9);
+        IntVar b = m.intVar("b", 0, 9);
+        IntVar c = m.intVar("c", 0, 9);
+        // 100a + 10b + c >= a * b * c
+        a.mul(100).add(b.mul(10)).add(c).ge(a.mul(b).mul(c)).post();
+        m.allDifferent(a, b, c).post();
+        boolean answer = m.getSolver().solve();
+        m.getSolver().printStatistics();
+        Assert.assertTrue(answer);
+    }
 }
