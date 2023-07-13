22c9ed1d552416af3f605ed7252dfe91e43e5673
cprudhom <charles.prudhomme@imt-atlantique.fr>
2023-07-13 11:28:18 +0200

    Add new value selector: closest to target (default target is 0)

---
+++ solver/src/main/java/org/chocosolver/solver/search/strategy/selectors/values/IntDomainClosest.java
@@ -0,0 +1,61 @@
+/*
+ * This file is part of choco-solver, http://choco-solver.org/
+ *
+ * Copyright (c) 2023, IMT Atlantique. All rights reserved.
+ *
+ * Licensed under the BSD 4-clause license.
+ *
+ * See LICENSE file in the project root for full license information.
+ */
+package org.chocosolver.solver.search.strategy.selectors.values;
+
+import org.chocosolver.solver.variables.IntVar;
+
+/**
+ * A value selector which selects the closest value to a given target value.
+ * <br/>
+ *
+ * @author Charles Prud'homme
+ * @since 10/07/2023
+ */
+public class IntDomainClosest implements IntValueSelector {
+
+    final int target;
+
+    /**
+     * Creates an IntDomainClosest value selector.
+     * The selected value is the closest to zero.
+     */
+    public IntDomainClosest() {
+        this(0);
+    }
+
+    /**
+     * Creates an IntDomainClosest value selector.
+     * The selected value is the closest to {@code target}.
+     *
+     * @param target the target value
+     */
+    public IntDomainClosest(int target) {
+        this.target = target;
+    }
+
+    @Override
+    public int selectValue(IntVar var) {
+        int pos = var.nextValue(target - 1);
+        int neg = var.previousValue(target + 1);
+        if (pos < Integer.MAX_VALUE) {
+            if (neg > Integer.MIN_VALUE) {
+                return pos + neg < 2 * target ? pos : neg;
+            } else {
+                return pos;
+            }
+        } else if (neg > Integer.MIN_VALUE) {
+            return neg;
+        } else {
+            // no value left -- should not happen
+            throw new UnsupportedOperationException();
+        }
+    }
+
+}
--- solver/src/test/java/org/chocosolver/solver/search/strategy/selectors/values/IntDomainClosestTest.java
+++ solver/src/test/java/org/chocosolver/solver/search/strategy/selectors/values/IntDomainClosestTest.java
@@ -20,16 +20,16 @@ import org.testng.annotations.Test;
  * <br/>
  *
  * @author Charles Prud'homme
- * @since 25/04/2019
+ * @since 13/07/2023
  */
-public class IntDomainMiddleTest {
+public class IntDomainClosestTest {
 
     @Test(timeOut = 60000, groups = "1s", expectedExceptions = ContradictionException.class)
     public void testSelectValue1() throws ContradictionException {
         Model model = new Model();
-        IntVar x = model.intVar("x", new int[]{0, 3, 5, 6, 7, 8, 11});
-        int[] order = {5, 6, 7, 3, 8, 0, 11};
-        IntDomainMiddle sel = new IntDomainMiddle(true);
+        IntVar x = model.intVar("x", new int[]{-6, -2, 0, 3, 5});
+        int[] order = {0, -2, 3, 5, -6};
+        IntDomainClosest sel = new IntDomainClosest();
         int i = 0;
         while (x.getDomainSize() > 0) {
             int val = sel.selectValue(x);
@@ -41,9 +41,9 @@ public class IntDomainMiddleTest {
     @Test(timeOut = 60000, groups = "1s", expectedExceptions = ContradictionException.class)
     public void testSelectValue2() throws ContradictionException {
         Model model = new Model();
-        IntVar x = model.intVar("x", new int[]{0, 3, 5, 6, 7, 8, 11});
-        int[] order = {7, 6, 8, 5, 3, 11, 0};
-        IntDomainMiddle sel = new IntDomainMiddle(v -> 7, true);
+        IntVar x = model.intVar("x", new int[]{-6, -2, 0, 3, 5});
+        int[] order = {3, 0, 5, -2, -6};
+        IntDomainClosest sel = new IntDomainClosest(2);
         int i = 0;
         while (x.getDomainSize() > 0) {
             int val = sel.selectValue(x);
@@ -51,4 +51,5 @@ public class IntDomainMiddleTest {
             x.removeValue(val, Cause.Null);
         }
     }
+
 }
