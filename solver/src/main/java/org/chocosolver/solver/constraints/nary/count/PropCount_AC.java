ea70f90c26ce8effc4da08ede8d944239023363f
Arthur Godet <arth.godet@gmail.com>
2024-01-09 13:29:12 +0100

    fix PropCount_AC wrt bounded domains

--- solver/src/main/java/org/chocosolver/solver/constraints/nary/count/PropCount_AC.java
+++ solver/src/main/java/org/chocosolver/solver/constraints/nary/count/PropCount_AC.java
@@ -76,7 +76,7 @@ public class PropCount_AC extends Propagator<IntVar> {
             st.append("..., ");
         }
         st.append("limit=").append(vars[vars.length - 1].getName());
-	    st.append(", value=").append(value).append(')');
+        st.append(", value=").append(value).append(')');
         return st.toString();
     }
 
@@ -112,15 +112,17 @@ public class PropCount_AC extends Propagator<IntVar> {
             if (possibles.contains(varIdx)) {
                 if (!vars[varIdx].contains(value)) {
                     possibles.remove(varIdx);
-                    filter();
+                    forcePropagate(PropagatorEventType.CUSTOM_PROPAGATION);
                 } else if (vars[varIdx].isInstantiated()) {
                     possibles.remove(varIdx);
                     mandatories.add(varIdx);
-                    filter();
+                    forcePropagate(PropagatorEventType.CUSTOM_PROPAGATION);
+                } else if (!vars[varIdx].hasEnumeratedDomain() && (vars[varIdx].getLB() == value || vars[varIdx].getLB() == value)) {
+                    forcePropagate(PropagatorEventType.CUSTOM_PROPAGATION);
                 }
             }
         } else {
-            filter();
+            forcePropagate(PropagatorEventType.CUSTOM_PROPAGATION);
         }
     }
 
