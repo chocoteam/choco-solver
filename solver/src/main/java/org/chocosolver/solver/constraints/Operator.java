dbf48faef4dd7b33681e6485b8bbdf1766cd018b
cprudhom <charles.prudhomme@imt-atlantique.fr>
2024-09-24 18:41:09 +0200

    Add better exceptions when an unknwon operator is declared

--- solver/src/main/java/org/chocosolver/solver/constraints/Operator.java
+++ solver/src/main/java/org/chocosolver/solver/constraints/Operator.java
@@ -10,6 +10,7 @@
 package org.chocosolver.solver.constraints;
 
 import gnu.trove.map.hash.THashMap;
+import org.chocosolver.solver.exception.SolverException;
 
 /**
  * <br/>
@@ -21,7 +22,7 @@ public enum Operator {
 
     NONE(), EQ(), LT(), GT(), NQ(), LE(), GE(), PL(), MN();
 
-	private static final THashMap<String, Operator> operators = new THashMap<>();
+    private static final THashMap<String, Operator> operators = new THashMap<>();
 
     static {
         operators.put("@", Operator.NONE);
@@ -35,50 +36,79 @@ public enum Operator {
         operators.put("-", Operator.MN);
     }
 
-	public static Operator get(String name) {
-        return operators.get(name);
-	}
+    public static Operator get(String name) {
+        Operator op = operators.get(name);
+        if (op == null) {
+            throw new SolverException("Operator \"" + name + "\" is not supported.\n" +
+                    "Supported operators are: " + operators.keySet() + "\n");
+        }
+        return op;
+    }
 
-	@Override
-	public String toString() {
-		switch (this){
-			case LT:return "<";
-			case GT:return ">";
-			case LE:return "<=";
-			case GE:return ">=";
-			case NQ:return "!=";
-			case EQ:return "=";
-			case PL:return "+";
-			case MN:return "-";
-			default:throw new UnsupportedOperationException();
-		}
-	}
+    @Override
+    public String toString() {
+        switch (this) {
+            case LT:
+                return "<";
+            case GT:
+                return ">";
+            case LE:
+                return "<=";
+            case GE:
+                return ">=";
+            case NQ:
+                return "!=";
+            case EQ:
+                return "=";
+            case PL:
+                return "+";
+            case MN:
+                return "-";
+            default:
+                throw new UnsupportedOperationException();
+        }
+    }
 
-	/**
-	 * Flips the direction of an inequality
-	 * @param operator op to flip
-	 */
-	public static String getFlip(String operator) {
-		switch (get(operator)){
-			case LT:return ">";
-			case GT:return "<";
-			case LE:return ">=";
-			case GE:return "<=";
-			default:return operator;
-		}
-	}
+    /**
+     * Flips the direction of an inequality
+     *
+     * @param operator op to flip
+     */
+    public static String getFlip(String operator) {
+        switch (get(operator)) {
+            case LT:
+                return ">";
+            case GT:
+                return "<";
+            case LE:
+                return ">=";
+            case GE:
+                return "<=";
+            default:
+                return operator;
+        }
+    }
 
-	public static Operator getOpposite(Operator operator) {
-		switch (operator){
-			case LT:return GE;
-			case GT:return LE;
-			case LE:return GT;
-			case GE:return LT;
-			case NQ:return EQ;
-			case EQ:return NQ;
-			case PL:return MN;
-			case MN:return PL;
-			default:throw new UnsupportedOperationException();
-		}
-	}
+    public static Operator getOpposite(Operator operator) {
+        switch (operator) {
+            case LT:
+                return GE;
+            case GT:
+                return LE;
+            case LE:
+                return GT;
+            case GE:
+                return LT;
+            case NQ:
+                return EQ;
+            case EQ:
+                return NQ;
+            case PL:
+                return MN;
+            case MN:
+                return PL;
+            default:
+                throw new UnsupportedOperationException();
+        }
+    }
 }
