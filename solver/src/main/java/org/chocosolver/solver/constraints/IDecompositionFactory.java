385a4d3ae0685fe97ff0f9d38dad0f708c5e2513
cprudhom <charles.prudhomme@imt-atlantique.fr>
2025-01-27 12:37:49 +0100

    Fix error in commit #20e72715

--- solver/src/main/java/org/chocosolver/solver/constraints/IDecompositionFactory.java
+++ solver/src/main/java/org/chocosolver/solver/constraints/IDecompositionFactory.java
@@ -124,8 +124,8 @@ public interface IDecompositionFactory extends ISelf<Model> {
         int d0 = matrix.length;
         int d1 = matrix[0].length;
         // Create the index as idx = row * d1 + col
-        IntVar rv = ref().intView(1, rowIndex, rowOffset);
-        IntVar cv = ref().intView(1, colIndex, colOffset);
+        IntVar rv = ref().intView(1, rowIndex, -rowOffset);
+        IntVar cv = ref().intView(1, colIndex, -colOffset);
         IntVar idx = ref().intVar(0, d0 * d1 - 1);
         ref().scalar(new IntVar[]{rv, cv}, new int[]{d1, 1}, "=", idx).post();
         // flatten the array
@@ -148,8 +148,8 @@ public interface IDecompositionFactory extends ISelf<Model> {
         int d0 = matrix.length;
         int d1 = matrix[0].length;
         // Create the index as idx = row * d1 + col
-        IntVar rv = ref().intView(1, rowIndex, rowOffset);
-        IntVar cv = ref().intView(1, colIndex, colOffset);
+        IntVar rv = ref().intView(1, rowIndex, -rowOffset);
+        IntVar cv = ref().intView(1, colIndex, -colOffset);
         IntVar idx = ref().intVar(0, d0 * d1 - 1);
         ref().scalar(new IntVar[]{rv, cv}, new int[]{d1, 1}, "=", idx).post();
         // flatten the array
