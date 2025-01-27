31378ab8a059f9467d1f4a35328a8d3f60e0521a
cprudhom <charles.prudhomme@imt-atlantique.fr>
2025-01-27 10:48:42 +0100

    Add InnerOuterCutoff as a new restart sequence

--- solver/src/main/java/org/chocosolver/solver/search/restart/GeometricalCutoff.java
+++ solver/src/main/java/org/chocosolver/solver/search/restart/GeometricalCutoff.java
@@ -20,16 +20,16 @@ package org.chocosolver.solver.search.restart;
  * @author Charles Prud'homme, Arnaud Malapert
  * @since 13/05/11
  */
-public final class GeometricalCutoff extends AbstractCutoff {
+public class GeometricalCutoff extends AbstractCutoff {
 
     /**
      * Declared geometrical factor
      */
-    private final double geometricalFactor;
+    protected final double geometricalFactor;
     /**
      * Current geometrical factor, after n calls to {@link #getNextCutoff()}.
      */
-    private double geometricalFactorPower;
+    protected double geometricalFactorPower;
 
     /**
      * A geometrical cutoff strategy.
