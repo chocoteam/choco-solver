8fd9c3c7efa5552853ad4df8c90b6e1a3babfa1b
cprudhom <charles.prudhomme@imt-atlantique.fr>
2025-03-28 14:32:49 +0100

    Remove PickOnFil.java

--- solver/src/test/java/org/chocosolver/solver/search/BlackBoxTest.java
+++ solver/src/test/java/org/chocosolver/solver/search/BlackBoxTest.java
@@ -42,7 +42,6 @@ public class BlackBoxTest {
                 {(Function<IntVar[], AbstractStrategy<IntVar>>) Search::failureRateBasedSearch},
                 {(Function<IntVar[], AbstractStrategy<IntVar>>) Search::failureLengthBasedSearch},
                 {(Function<IntVar[], AbstractStrategy<IntVar>>) Search::pickOnDom},
-                {(Function<IntVar[], AbstractStrategy<IntVar>>) Search::pickOnFil},
         };
     }
 
