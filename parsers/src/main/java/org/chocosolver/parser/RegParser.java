1003399df5711eac4f8b299b0491b4e9393de846
cprudhom <charles.prudhomme@imt-atlantique.fr>
2025-07-09 15:18:21 +0200

    Change parameters of RegParser

--- parsers/src/main/java/org/chocosolver/parser/RegParser.java
+++ parsers/src/main/java/org/chocosolver/parser/RegParser.java
@@ -95,8 +95,8 @@ public abstract class RegParser implements IParser {
 
     @Option(name = "-flush",
             forbids = {"-varsel"},
-            usage = "Autoflush weights on black-box strategies (default: 32).")
-    protected int flushRate = 32;
+            usage = "Autoflush weights on black-box strategies (default: 20).")
+    protected int flushRate = 20;
 
     @Option(name = "-varsel",
             handler = VarSelHandler.class,
@@ -166,7 +166,7 @@ public abstract class RegParser implements IParser {
     protected int nb_cores = 1;
 
     @Option(name = "-seed", usage = "Set the seed for random number generator. ")
-    protected long seed = 0L;
+    protected long seed = 1_000_000_007L;
 
     @Option(name = "--cp-profiler", usage = "Connect to CP-Profiler. Two comma-separated values are expected: the execution id and the port.")
     public String cpProfiler = null;
