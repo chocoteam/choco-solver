f77428e2c0b6a9ae41580d7bfc46d220cd28c59b
Jean-Guillaume Fages <jg.fages@cosling.com>
2025-12-01 16:54:28 +0100

    IntVar no longer extends Iterable (#1177)

--- solver/src/main/java/org/chocosolver/solver/variables/impl/IntVarLazyLit.java
+++ solver/src/main/java/org/chocosolver/solver/variables/impl/IntVarLazyLit.java
@@ -28,8 +28,6 @@ import org.chocosolver.util.iterators.DisposableValueIterator;
 import org.chocosolver.util.iterators.EvtScheduler;
 import org.chocosolver.util.objects.setDataStructures.iterable.IntIterableSet;
 
-import java.util.Iterator;
-
 import static org.chocosolver.sat.MiniSat.C_Undef;
 
 /**
@@ -359,11 +357,6 @@ public final class IntVarLazyLit extends AbstractVariable implements IntVar, Lit
     @Override
     public IIntDeltaMonitor monitorDelta(ICause propagator) {
         return var.monitorDelta(propagator);
-    }
-
-    @Override
-    public Iterator<Integer> iterator() {
-        return var.iterator();
     }
 
     @Override
