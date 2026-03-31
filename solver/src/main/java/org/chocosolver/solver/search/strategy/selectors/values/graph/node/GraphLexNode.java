932d6d61ca008478cb97ef8a3b2d567068fc150f
Charles Prud'homme <charles.prudhomme@imt-atlantique.fr>
2026-03-31 17:21:25 +0200

    License : move to BSD 3-clause

--- solver/src/main/java/org/chocosolver/solver/search/strategy/selectors/values/graph/node/GraphLexNode.java
+++ solver/src/main/java/org/chocosolver/solver/search/strategy/selectors/values/graph/node/GraphLexNode.java
@@ -1,10 +1,7 @@
 /*
  * This file is part of choco-solver, http://choco-solver.org/
- *
- * Copyright (c) 2021, IMT Atlantique. All rights reserved.
- *
- * Licensed under the BSD 4-clause license.
- *
+ * Copyright (c) 2001, IMT Atlantique.
+ * SPDX-License-Identifier: BSD-3-Clause.
  * See LICENSE file in the project root for full license information.
  */
 package org.chocosolver.solver.search.strategy.selectors.values.graph.node;
